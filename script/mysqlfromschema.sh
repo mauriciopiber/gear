9a23317afe544a4f4a158e0c33e5d67225054617
Mauricio Piber <mauriciopiber@gmail.com>
2014-10-20 07:47:04 +0000

    Agora sim vai pra staging essa porra!

--- script/mysqlfromschema.sh
+++ script/mysqlfromschema.sh
@@ -4,11 +4,14 @@ echo ${1}
 echo ${2}
 echo ${3}
 echo ${4}
+#exit 1;
 
 cd ${1}
-mysql -u ${3} -h localhost -p${4} -Bse "DROP DATABASE \`${2}\`;"
+#mysql -u ${3} -h localhost -p${4} -Bse "DROP DATABASE \`${2}\`;"
 mysql -u ${3} -h localhost -p${4} -Bse "CREATE DATABASE IF NOT EXISTS  \`${2}\`;"
 
+#php app/console doctrine:database:create
 vendor/bin/doctrine-module orm:validate-schema
-vendor/bin/doctrine-module orm:schema-tool:update --force
+vendor/bin/doctrine-module orm:schema-tool:create
+#vendor/bin/doctrine-module orm:schema-tool:update --force
 vendor/bin/doctrine-module orm:validate-schema
