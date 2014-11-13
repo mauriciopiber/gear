7dbe4984c476436346486c3f4c9d2ef2c683d529
Maurício Piber <mauriciopiber@gmail.com>
2014-11-13 16:39:29 +0000

    change

--- config/module.config.php
+++ config/module.config.php
@@ -14,7 +14,7 @@ return array(
     'service_manager' => $serviceManager,
     'acl'     => array('Gear' => true),
     'url'     => 'modules.gear.dev',
-    'version' => '0.1.17',
+    'version' => '0.1.18',
     'console' => $consoleRoutes,
     'controllers' => array(
         'invokables' => array(
