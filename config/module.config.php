678d2b4267a02cc8bcbe795730b0434d1166f7f1
Maurício Piber <mauriciopiber@gmail.com>
2014-11-13 15:53:06 +0000

    Teste

--- config/module.config.php
+++ config/module.config.php
@@ -14,7 +14,7 @@ return array(
     'service_manager' => $serviceManager,
     'acl'     => array('Gear' => true),
     'url'     => 'modules.gear.dev',
-    'version' => '0.1.13',
+    'version' => '0.1.14',
     'console' => $consoleRoutes,
     'controllers' => array(
         'invokables' => array(
