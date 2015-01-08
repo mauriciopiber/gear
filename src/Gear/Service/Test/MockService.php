35b8aa5553f78ed4439b5fb1c434159b408a8407
Maurício Piber <mauriciopiber@gmail.com>
2015-01-08 14:54:43 +0000

    Entity ok porra!

---
+++ src/Gear/Service/Test/MockService.php
@@ -0,0 +1,15 @@
+<?php
+namespace Gear\Service\Test;
+
+use Gear\Service\AbstractService;
+
+class MockService extends AbstractService
+{
+
+
+
+    public function getMockForTableStructure() {
+
+    }
+
+}
