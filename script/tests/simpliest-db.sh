e6ea5d611f4740254030572dd37b7b6903c34939
Maurício Piber <mauriciopiber@gmail.com>
2015-02-06 03:57:50 +0000

    Funcionando finalmente os 3 contextos, agora precisa testar com precissão cirurgica essa porra!

--- script/tests/simpliest-db.sh
+++ script/tests/simpliest-db.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-php ../../public/index.php gear module unload BjyAuthorize
+#php ../../public/index.php gear module unload BjyAuthorize
 php ../../public/index.php gear module delete Teste
 php ../../public/index.php gear module create Teste
 php ./../../public/index.php gear db create Teste --table="Email" --user="low-strict"
+php ../../public/index.php gear src create Teste --type="Entity" --name="User" --db="User"
+php ../../public/index.php gear src create Teste --type="Entity" --name="Role" --db="Role"
+exit 1;
 #php ../../public/index.php gear module build Teste --trigger=phpcs
 
 #exit 1;
-php ../../public/index.php gear src create Teste --type="Entity" --name="User" --db="User"
-php ../../public/index.php gear src create Teste --type="Entity" --name="Role" --db="Role"
+
 php ./../../public/index.php gear src create Teste --type="Fixture" --name="Role" --db="Role"
 php ./../../public/index.php gear src create Teste --type="Fixture" --name="User" --db="User"
 
