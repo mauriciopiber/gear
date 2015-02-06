664b102183ab1a49945d6909c052972ba731981d
Maurício Piber <mauriciopiber@gmail.com>
2015-02-06 04:36:36 +0000

    Funcionando finalmente os 3 contextos, agora precisa testar com precissão cirurgica essa porra!2

---
+++ src/Gear/Creator/File.php
@@ -0,0 +1,161 @@
+<?php
+namespace Gear\Creator;
+
+use Doctrine\Common\Collections\ArrayCollection;
+class File
+{
+
+    protected $view;
+
+    protected $options;
+
+    protected $fileName;
+
+    protected $location;
+
+    protected $childView;
+
+    protected $fileService;
+
+    protected $templateService;
+
+    public function __construct($fileService, $templateService)
+    {
+        $this->fileService = $fileService;
+        $this->templateService = $templateService;
+    }
+
+    public function render()
+    {
+        if (!$this->view) {
+            throw new \Gear\Exception\FileCreator\ViewNotFoundException();
+        }
+
+        if (!$this->options) {
+            throw new \Gear\Exception\FileCreator\ConfigNotFoundException();
+        }
+
+        if (!$this->fileName) {
+            throw new \Gear\Exception\FileCreator\FileNameNotFoundException();
+        }
+
+        if (!$this->location) {
+            throw new \Gear\Exception\FileCreator\LocationNotFoundException();
+        }
+
+        $view = $this->renderViewModel($this->getRenderView());
+        return $this->fileService->factory($this->getLocation(), $this->getFileName(), $view);
+    }
+
+    public function renderViewModel($viewModel)
+    {
+        $viewModel->setOption('has_parent', true);
+
+        $renderer = $this->templateService->getRenderer();
+
+        if ($viewModel->hasChildren()) {
+            foreach ($viewModel->getChildren() as $child) {
+                if($viewModel->terminate() && $child->terminate()) {
+                    throw new DomainException('Inconsistent state; child view model is marked as terminal');
+                }
+                $child->setOption('has_parent', true);
+                $result = $this->renderViewModel($child);
+                $child->setOption('has_parent', null);
+                $capture = $child->captureTo();
+                if (!empty($capture)) {
+                    if ($child->isAppend()) {
+                        $oldResult=$viewModel->{$capture};
+                        $viewModel->setVariable($capture, $oldResult . $result);
+                    } else {
+                        $viewModel->setVariable($capture, $result);
+                    }
+                }
+            }
+        }
+
+        $html = $renderer->render($viewModel);
+        return $html;
+    }
+
+    public function getRenderView()
+    {
+        $viewModel = new ViewModel($this->getOptions());
+        $viewModel->setTemplate($this->getView());
+
+        if ($this->getChildView() !== null && $this->getChildView()->count() > 0) {
+
+            foreach ($this->getChildView() as $i => $child) {
+
+                $childViewModel = new ViewModel($child['config'***REMOVED***);
+                $childViewModel->setTemplate($child['template'***REMOVED***);
+
+
+                $viewModel->addChild($childViewModel, $child['placeholder'***REMOVED***);
+            }
+        }
+        return $viewModel;
+    }
+
+    /**
+     *
+     * @param array $view -> template -> config -> placeholder
+     */
+    public function addChildView($view)
+    {
+        if (!$this->childView) {
+            $this->childView = new ArrayCollection();
+        }
+        $this->childView->add($view);
+    }
+
+    public function getChildView()
+    {
+        return $this->childView;
+    }
+
+
+
+    public function getView()
+    {
+        return $this->view;
+    }
+
+    public function setView($view)
+    {
+        $this->view = $view;
+        return $this;
+    }
+
+    public function getOptions()
+    {
+        return $this->options;
+    }
+
+    public function setOptions($config)
+    {
+        $this->options = $config;
+        return $this;
+    }
+
+    public function getFileName()
+    {
+        return $this->fileName;
+    }
+
+    public function setFileName($fileName)
+    {
+        $this->fileName = $fileName;
+        return $this;
+    }
+
+    public function getLocation()
+    {
+        return $this->location;
+    }
+
+    public function setLocation($location)
+    {
+        $this->location = $location;
+        return $this;
+    }
+}
