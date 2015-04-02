2bb0eeb7d20e6ffe11f2fdeb5a4bd47e4aa15ce7
Maurício Piber <mauriciopiber@gmail.com>
2015-04-02 01:43:54 +0000

    Adicionado PHPMD

--- script/tests/simpliest-db-low-strict.sh
+++ script/tests/simpliest-db-low-strict.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
 
-php ../../public/index.php gear module unload BjyAuthorize
-php ../../public/index.php gear module delete Teste
-php ../../public/index.php gear module create Teste
+php ./../../public/index.php gear module unload BjyAuthorize
+php ./../../public/index.php gear module delete Teste
+php ./../../public/index.php gear module create Teste
 php ./../../public/index.php gear db create Teste --table="Email" --user="low-strict"
-#--user="low-strict"
-#php ../../public/index.php gear src create Teste --type="Entity" --name="User" --db="User"
-#php ../../public/index.php gear src create Teste --type="Entity" --name="Role" --db="Role"
-#php ./../../public/index.php gear src create Teste --type="Fixture" --name="Role" --db="Role"
-#php ./../../public/index.php gear src create Teste --type="Fixture" --name="User" --db="User"
 
-php ../../public/index.php gear project fixture --reset-increment
-php ../../public/index.php gear project setUpAcl
+
+php ./../../public/index.php gear project fixture --reset-increment
+php ./../../public/index.php gear project setUpAcl
 /usr/bin/expect ./script/utils/clear-memcached.sh
-php ../../public/index.php gear module load BjyAuthorize --before=ZfcBase
-php ../../public/index.php gear module build Teste --trigger=phpunit
+php ./../../public/index.php gear module load BjyAuthorize --before=ZfcBase
+php ./../../public/index.php gear database mysql dump /var/www/html/modules/module/Teste/data/ teste.mysql.sql
+php ./../../public/index.php gear module build Teste --trigger=phpunit
 
 
 exit 1
