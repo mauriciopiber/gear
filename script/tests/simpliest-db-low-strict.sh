6edb1dac4cc3bb315475f240ddcadfa407796f3f
Maurício Piber <mauriciopiber@gmail.com>
2015-04-06 03:40:03 +0000

    Corrigido acceptance, functional, phpcs, phpmd

--- script/tests/simpliest-db-low-strict.sh
+++ script/tests/simpliest-db-low-strict.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 
-#php ./../../public/index.php gear module unload BjyAuthorize
-#php ./../../public/index.php gear module delete Teste
-#php ./../../public/index.php gear module create Teste
+php ./../../public/index.php gear module unload BjyAuthorize
+php ./../../public/index.php gear module delete Teste
+php ./../../public/index.php gear module create Teste
 php ./../../public/index.php gear db create Teste --table="Email" --user="low-strict"
-php ./../../public/index.php gear module build Teste --trigger=phpcs
+#php ./../../public/index.php gear module build Teste --trigger=phpcs
 
-exit 1
+
 
 php ./../../public/index.php gear project fixture --reset-increment
 php ./../../public/index.php gear project setUpAcl
 /usr/bin/expect ./script/utils/clear-memcached.sh
 php ./../../public/index.php gear module load BjyAuthorize --before=ZfcBase
 php ./../../public/index.php gear database mysql dump /var/www/html/modules/module/Teste/data/ teste.mysql.sql
-php ./../../public/index.php gear module build Teste --trigger=phpcs
+
+
+exit 1
+
+php ./../../public/index.php gear module build Teste --trigger=acceptance
+php ./../../public/index.php gear module build Teste --trigger=functional
 
 
 exit 1
