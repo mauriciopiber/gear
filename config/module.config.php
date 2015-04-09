1c312da488321ab6f8c010829f8e388633e6e715
Maurício Piber <mauriciopiber@gmail.com>
2015-04-09 22:02:44 +0000

    Dando

--- config/module.config.php
+++ config/module.config.php
@@ -15,10 +15,10 @@ return array(
     ),
     'speciality' => $speciality,
     'service_manager' => $serviceManager,
-    'gear' => array('version' => '0.1.42', 'acl' => true, 'name' => __NAMESPACE__),
+    'gear' => array('version' => '0.1.43', 'acl' => true, 'name' => __NAMESPACE__),
     'acl'     => array('Gear' => true),
     'url'     => 'modules.gear.dev',
-    'version' => '0.1.42',
+    'version' => '0.1.43',
     'console' => $consoleRoutes,
     'controllers' => array(
         'invokables' => array(
