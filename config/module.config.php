4799cf33978bfad54869d23aea6b8a1a23c9a4dd
Maurício Piber <mauriciopiber@gmail.com>
2015-04-09 22:12:14 +0000

    VAMOOO

--- config/module.config.php
+++ config/module.config.php
@@ -15,10 +15,10 @@ return array(
     ),
     'speciality' => $speciality,
     'service_manager' => $serviceManager,
-    'gear' => array('version' => '0.1.45', 'acl' => true, 'name' => __NAMESPACE__),
+    'gear' => array('version' => '0.1.46', 'acl' => true, 'name' => __NAMESPACE__),
     'acl'     => array('Gear' => true),
     'url'     => 'modules.gear.dev',
-    'version' => '0.1.45',
+    'version' => '0.1.46',
     'console' => $consoleRoutes,
     'controllers' => array(
         'invokables' => array(
