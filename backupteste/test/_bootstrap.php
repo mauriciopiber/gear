154dc21531ae5171b91d484c3dcc5f871f479145
Maurício Piber <mauriciopiber@gmail.com>
2015-04-15 08:49:05 +0000

    funcionando integração porra

--- backupteste/test/_bootstrap.php
+++ backupteste/test/_bootstrap.php
@@ -1,5 +1,6 @@
 <?php
+include 'ZendServiceLocator.php';
 // This is global bootstrap for autoloading
-
+$zendServiceLocator = new \Teste\ZendServiceLocator();
 \Codeception\Util\Autoload::addNamespace('Teste\Pages', __DIR__.DIRECTORY_SEPARATOR.'_pages');
 require_once '_support/LoginCommons.php';
