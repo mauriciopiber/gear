981a1d342e3fed1f3218fbecc53dbcf7994c8c1d
Maurício Piber <mauriciopiber@gmail.com>
2015-04-15 09:24:36 +0000

    Garantindo

--- config/module.config.php
+++ config/module.config.php
@@ -15,10 +15,10 @@ return array(
     ),
     'speciality' => $speciality,
     'service_manager' => $serviceManager,
-    'gear' => array('version' => '0.1.50', 'acl' => true, 'name' => __NAMESPACE__),
+    'gear' => array('version' => '0.1.51', 'acl' => true, 'name' => __NAMESPACE__),
     'acl'     => array('Gear' => true),
     'url'     => 'modules.gear.dev',
-    'version' => '0.1.50',
+    'version' => '0.1.51',
     'console' => $consoleRoutes,
     'controllers' => array(
         'invokables' => array(
