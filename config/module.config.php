fc275731be1867783500f3ca022d2c2d5685f457
Maurício Piber <mauriciopiber@gmail.com>
2015-04-16 08:41:35 +0000

    Fixing

--- config/module.config.php
+++ config/module.config.php
@@ -15,10 +15,10 @@ return array(
     ),
     'speciality' => $speciality,
     'service_manager' => $serviceManager,
-    'gear' => array('version' => '0.1.54', 'acl' => true, 'name' => __NAMESPACE__),
+    'gear' => array('version' => '0.1.55', 'acl' => true, 'name' => __NAMESPACE__),
     'acl'     => array('Gear' => true),
     'url'     => 'modules.gear.dev',
-    'version' => '0.1.54',
+    'version' => '0.1.55',
     'console' => $consoleRoutes,
     'controllers' => array(
         'invokables' => array(
