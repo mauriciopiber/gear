29bb0560dbbb1d1ccc371712ae63c065c4fb6b38
Maurício Piber <mauriciopiber@gmail.com>
2015-04-26 21:23:01 +0000

    vamo

--- script/final/all-columns.sh
+++ script/final/all-columns.sh
@@ -16,22 +16,23 @@ varcharuniqueid="\"column_varchar_unique_id\": \"unique-id\""
 
 columns="{$datetimeptbr, $dateptbr, $decimalptbr, $intcheckbox, $tinyintcheckbox, $varcharemail, $varcharpasswordverify, $varcharimageupload, $varcharuniqueid}"
 
-php $index gear module unload BjyAuthorize
+#php $index gear module unload BjyAuthorize
 
-php $index gear module delete Column
+#php $index gear module delete Column
 
-php $index gear module create Column
+#php $index gear module create Column
 
 php $index gear module db create Column --table="Columns" --columns="$columns"
-php $index gear module db create Column --table="ForeignKeys"
+#php $index gear module db create Column --table="ForeignKeys"
 
-php $index gear project resetAcl
-php $index gear project fixture --reset-autoincrement
-php $index gear project setUpAcl
-/usr/bin/expect ./script/utils/clear-memcached.sh
-php $index gear module load BjyAuthorize --before=ZfcBase
+#php $index gear project resetAcl
+#php $index gear project fixture --reset-autoincrement
+#php $index gear project setUpAcl
+#/usr/bin/expect ./script/utils/clear-memcached.sh
+#php $index gear module load BjyAuthorize --before=ZfcBase
 
-php $index gear database mysql dump /var/www/html/modules/module/Column/data/ column.mysql.sql
-
+#php $index gear database mysql dump /var/www/html/modules/module/Column/data/ column.mysql.sql
+#php $index gear module build Column --trigger=acceptance-set --domain=ColumnsViewCest
+php $index gear module build Column --trigger=file-check --domain=test/acceptance/ColumnsViewCest.php
 #php $index gear module build Column --trigger=phpunit-group --domain=Controller.Create
 #php $index gear module build Column --trigger=phpunit
