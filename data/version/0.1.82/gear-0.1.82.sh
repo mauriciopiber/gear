cd4b3f059d150c0e761e8c596cfbb499fe1f51c1
Maurício Piber <mauriciopiber@gmail.com>
2015-05-12 04:41:47 +0000

    vamo ajeita essa porra

--- data/version/0.1.82/gear-0.1.82.sh
+++ data/version/0.1.82/gear-0.1.82.sh
@@ -6,11 +6,10 @@ index="$base/public/index.php"
 php $index gear module delete FreeMind
 php $index gear module create FreeMind
 
-php $index gear module src create FreeMind --name="Mauricio" --namespace="Piber" --yes
-php $index gear module src create FreeMind --name="Mauricio" --namespace="Fao" --yes
-php $index gear module src create FreeMind --name="Mauricio" --namespace="Buceta" --yes
-php $index gear module src create FreeMind --name="Mauricio" --namespace="Bucetao" --yes
-php $index gear module src create FreeMind --name="Mauricio" --namespace="Bucetildes" --yes
+php $index gear module src create FreeMind --type="Service" --name="MyService"
+php $index gear module src create FreeMind --type="Repository" --name="MyRepository"
+php $index gear module src create FreeMind --type="Form" --name="MyRepository"
+php $index gear module src create FreeMind --type="Filter" --name="MyFilter"
 
 
 php $index gear database mysql dump /var/www/html/modules/module/FreeMind/data/ free-mind.mysql.sql
@@ -19,3 +18,5 @@ php $index gear module build FreeMind --trigger=unit-coverage
 php $index gear module build FreeMind --trigger=phpcs
 php $index gear module build FreeMind --trigger=phpmd
 php $index gear module build FreeMind --trigger=phpcpd
+
+
