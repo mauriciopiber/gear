47bcaa1530f2ac945ff7dfb013aaf92b0f91bf8b
Maurício Piber <mauriciopiber@gmail.com>
2015-05-12 04:35:56 +0000

    Criado

--- config/module.config.php
+++ config/module.config.php
@@ -15,10 +15,10 @@ return array(
     ),
     'speciality' => $speciality,
     'service_manager' => $serviceManager,
-    'gear' => array('version' => '0.1.80', 'acl' => true, 'name' => __NAMESPACE__),
+    'gear' => array('version' => '0.1.81', 'acl' => true, 'name' => __NAMESPACE__),
     'acl'     => array('Gear' => true),
     'url'     => 'modules.gear.dev',
-    'version' => '0.1.80',
+    'version' => '0.1.81',
     'console' => $consoleRoutes,
     'controllers' => array(
         'invokables' => array(
