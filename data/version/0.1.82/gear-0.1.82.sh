675f3f5f1ba3b306f76e261fe2c212506ebe1567
Maurício Piber <mauriciopiber@gmail.com>
2015-05-12 04:37:06 +0000

    Começando 0.1.82

---
+++ data/version/0.1.82/gear-0.1.82.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+base="/var/www/html/modules"
+index="$base/public/index.php"
+
+php $index gear module delete FreeMind
+php $index gear module create FreeMind
+
+php $index gear module src create FreeMind --name="Mauricio" --namespace="Piber" --yes
+php $index gear module src create FreeMind --name="Mauricio" --namespace="Fao" --yes
+php $index gear module src create FreeMind --name="Mauricio" --namespace="Buceta" --yes
+php $index gear module src create FreeMind --name="Mauricio" --namespace="Bucetao" --yes
+php $index gear module src create FreeMind --name="Mauricio" --namespace="Bucetildes" --yes
+
+
+php $index gear database mysql dump /var/www/html/modules/module/FreeMind/data/ free-mind.mysql.sql
+
+php $index gear module build FreeMind --trigger=unit-coverage
+php $index gear module build FreeMind --trigger=phpcs
+php $index gear module build FreeMind --trigger=phpmd
+php $index gear module build FreeMind --trigger=phpcpd
