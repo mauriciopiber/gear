e25cc93723d1b8b48b22fe7ca946ddbd1382373e
Maurício Piber <mauriciopiber@gmail.com>
2015-05-13 01:33:37 +0000

    vamo cara

--- data/version/0.1.82/gear-0.1.82.sh
+++ data/version/0.1.82/gear-0.1.82.sh
@@ -8,10 +8,13 @@ php $index gear module unload BjyAuthorize
 php $index gear module delete FreeMind
 php $index gear module create FreeMind
 
+php $index gear module src create FreeMind --type="Service" --name="OtherService"
+php $index gear module src create FreeMind --type="Service" --name="AnotherService"
+php $index gear module src create FreeMind --type="Repository" --name="OtherRepository"
 
 php $index gear module src create FreeMind --type="Repository" --name="MyRepository"
 php $index gear module src create FreeMind --type="Repository" --name="MyRepositoryDependency" --dependency="Service\OtherService"
-php $index gear module src create FreeMind --type="Repository" --name="MyRepositoryMultiDependency" --dependency="Repository\OtherRepository"
+php $index gear module src create FreeMind --type="Repository" --name="MyRepositoryMultiDependency" --dependency="Service\OtherService,Service\AnotherService,Repository\OtherRepository"
 php $index gear module src create FreeMind --type="Repository" --name="MyRepositoryExtends" --extends="GearBase\AbstractBase"
 php $index gear module src create FreeMind --type="Repository" --name="MyRepositoryDependencyExtends" --extends="GearBase\AbstractBase" --dependency="Service\OtherService"
 #php $index gear module src create FreeMind --type="Repository" --name="MyRepositoryDependencyExtends"
@@ -21,7 +24,9 @@ php $index gear module src create FreeMind --type="Repository" --name="MyReposit
 #php $index gear module src create FreeMind --type="Repository" --name="ColumnsRepository" --db="Columns"
 
 php $index gear database mysql dump /var/www/html/modules/module/FreeMind/data/ free-mind.mysql.sql
-
+php $index gear module build FreeMind --trigger=phpcs
+php $index gear module build FreeMind --trigger=phpmd
+php $index gear module build FreeMind --trigger=phpcpd
 php $index gear module build FreeMind --trigger=unit-coverage
 rm $base/public/freemind-coverage
 ln -s $base/module/FreeMind/build/coverage/coverage $base/public/freemind-coverage
@@ -47,8 +52,5 @@ php $index gear database mysql dump /var/www/html/modules/module/FreeMind/data/ 
 
 php $index gear module build FreeMind --trigger=unit-coverage
 exit 1
-php $index gear module build FreeMind --trigger=phpcs
-php $index gear module build FreeMind --trigger=phpmd
-php $index gear module build FreeMind --trigger=phpcpd
 
 
