e172c116ea659c1ba1503c1822ed64829aed208f
Maurício Piber <mauriciopiber@gmail.com>
2015-05-15 23:39:10 +0000

    vamo ve

--- data/version/0.1.83/gear-0.1.83-3.sh
+++ data/version/0.1.83/gear-0.1.83-3.sh
@@ -7,39 +7,34 @@ module=VaiTomaNoCu
 php $index gear module delete $module
 php $index gear module create $module
 
-
 php $index gear project setUpAcl
-
 php $index gear database mysql dump /var/www/html/modules/module/$module/data/ vai-toma-no-cu.mysql.sql
-
-
-php $index gear module test create $module --suite=acceptance --target=MyCaaaAcceptanceCest.php
-php $index gear module test create $module --suite=acceptance --target=MyAaaaAcceptanceCest.php
-php $index gear module test create $module --suite=acceptance --target=MyBaaaAcceptanceCest.php
-php $index gear module test create $module --suite=acceptance --target=MyDaaaAcceptanceCest.php
-
-
-#php $index gear module test create $module --suite=functional --target=MyFirstFunctionalCest.php
-#php $index gear module test create $module --suite=unit --target=MyFirstUnitTest.php
-
-
-ls -l $base/module/$module/test/acceptance
-echo ""
-cat $base/module/$module/test/acceptance/MyFirstAcceptanceCest.php
-echo ""
-echo ""
-
 php $index gear cache renew --memcached --data
 
-php $index gear module build $module --trigger=acceptance-set --domain="ModuleMainPageCest"
+
+#php $index gear module test create $module --suite=acceptance --target=MyAaaaAcceptanceCest.php
+#php $index gear module test create $module --suite=acceptance --target=MyBaaaAcceptanceCest.php
+#php $index gear module test create $module --suite=acceptance --target=MyCaaaAcceptanceCest.php
+#php $index gear module test create $module --suite=acceptance --target=MyDaaaAcceptanceCest.php
+#php $index gear module build $module --trigger=acceptance
+
+#php $index gear module test create $module --suite=functional --target=MyAaaaFunctionalCest.php
+#php $index gear module test create $module --suite=functional --target=MyBaaaFunctionalCest.php
+#php $index gear module test create $module --suite=functional --target=MyCaaaFunctionalCest.php
+#php $index gear module test create $module --suite=functional --target=MyDaaaFunctionalCest.php
+#php $index gear module build $module --trigger=functional
+
+
+php $index gear module test create $module --suite=unit --target=MyTest/MyAaaaUnitTest.php
+php $index gear module test create $module --suite=unit --target=MySecond/MyBaaaUnitTest.php
+php $index gear module test create $module --suite=unit --target=MyCaaaUnitTest.php
+php $index gear module test create $module --suite=unit --target=MyDaaaUnitTest.php
+ls -l $base/module/$module/test/unit
+ls -l $base/module/$module/test/unit/TestingTest
+
+php $index gear module build $module --trigger=unit
 
 exit 1
-
-ls -l $base/module/$module/test/functional
-echo ""
-cat $base/module/$module/test/functional/MyFirstFunctionalCest.php
-echo ""
-echo ""
 
 
 ls -l $base/module/$module/test/unit/TestingTest
