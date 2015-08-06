e7327b829061a7000ed46389522c69ef47a1fd4b
root <root@gear.dev>
2015-08-06 17:36:50 +0000

    Limpando tudo que nao presta

--- script/jobs/contact.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-php ./../../public/index.php gear module delete Contact
-
-php ./../../public/index.php gear module create Contact
--- script/jobs/file-upload.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-php ./../../public/index.php gear module delete FileUpload
-
-php ./../../public/index.php gear module create FileUpload
