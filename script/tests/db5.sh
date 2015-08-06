658fbc881aa7d0892b15f1ede3160e6fe627b6f6
root <root@gear.dev>
2015-08-06 17:36:50 +0000

    Limpando tudo que nao presta

--- script/tests/db5.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-php ./../../public/index.php gear module delete Admin
-
-php ./../../public/index.php gear module create Admin
-
-php ./../../public/index.php gear db create Admin --table="Module"
-php ./../../public/index.php gear db create Admin --table="Controller"
--- script/tests/src4.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-php ./../../public/index.php gear module delete Admin
-php ./../../public/index.php gear module create Admin
-
-php ./../../public/index.php gear src create Admin --type="Service" --name="MyService"
-php ./../../public/index.php gear build Admin dev
