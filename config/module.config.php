078af2cfd01d34ba6c3f806bf9a17278aa3fca24
root <root@gear.dev>
2015-08-11 00:56:39 +0000

    Testar

--- config/module.config.php
+++ config/module.config.php
@@ -15,10 +15,10 @@ return array(
     ),
     'speciality' => $speciality,
     'service_manager' => $serviceManager,
-    'gear' => array('version' => '0.1.93', 'acl' => true, 'name' => __NAMESPACE__),
+    'gear' => array('version' => '0.1.94', 'acl' => true, 'name' => __NAMESPACE__),
     'acl'     => array('Gear' => true),
     'url'     => 'modules.gear.dev',
-    'version' => '0.1.93',
+    'version' => '0.1.94',
     'console' => $consoleRoutes,
     'controllers' => array(
         'invokables' => array(
