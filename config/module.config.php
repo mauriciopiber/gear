c7c1cf08f25a6bca68512db646a0ee1bf8161d84
root <root@gear.dev>
2015-09-02 17:21:06 -0300

    Adicionado

--- config/module.config.php
+++ config/module.config.php
@@ -15,10 +15,10 @@ return array(
     ),
     'speciality' => $speciality,
     'service_manager' => $serviceManager,
-    'gear' => array('version' => '0.1.106', 'acl' => true, 'name' => __NAMESPACE__),
+    'gear' => array('version' => '0.1.107', 'acl' => true, 'name' => __NAMESPACE__),
     'acl'     => array('Gear' => true),
     'url'     => 'modules.gear.dev',
-    'version' => '0.1.106',
+    'version' => '0.1.107',
     'console' => $consoleRoutes,
     'controllers' => array(
         'invokables' => array(
