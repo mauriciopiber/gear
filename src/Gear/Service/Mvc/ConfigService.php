940bb8508905585341ba10b425f8e3c5f412e070
mauriciopiber <mauriciopiber@gmail.com>
2015-10-05 17:47:08 -0300

    Atualização

--- src/Gear/Service/Mvc/ConfigService.php
+++ src/Gear/Service/Mvc/ConfigService.php
@@ -375,8 +375,60 @@ EOS;
     }
 
 
+    public function triggerMergeServiceManager()
+    {
+        $serviceManager = new \Gear\Config\ServiceManager($this->getModule());
+        $serviceManager->extractServiceManagerFromSrc($this->src);
+
+        if (!isset($this->serviceManagerFile[$serviceManager->getPattern()***REMOVED***)) {
+            $this->serviceManagerFile[$serviceManager->getPattern()***REMOVED*** = [***REMOVED***;
+        }
+
+        if (array_key_exists($serviceManager->getCallable(), $this->serviceManagerFile['invokables'***REMOVED***)) {
+            return;
+        }
+
+        $data = $serviceManager->getArray();
+
+        if ($serviceManager->getPattern() == 'invokables') {
+
+            $this->serviceManagerFile['invokables'***REMOVED***[sprintf('%s\%s\%s', $this->getModule()->getModuleName(), $this->src->getType(), $this->src->getName())***REMOVED*** =
+                sprintf('%s\%s\%s', $this->getModule()->getModuleName(), $this->src->getType(), (isset($data['invokables'***REMOVED***[0***REMOVED***['aliase'***REMOVED***) ? $data['invokables'***REMOVED***[0***REMOVED***['aliase'***REMOVED*** : $this->src->getName()));
+
+             $this->arrayToFile($this->getModule()->getConfigExtFolder().'/servicemanager.config.php', $this->serviceManagerFile);
+
+             return;
+        }
+
+        if ($serviceManager->getPattern() == 'factories') {
+             $this->serviceManagerFile['factories'***REMOVED***[$data['factories'***REMOVED***[0***REMOVED***['callable'***REMOVED******REMOVED*** = $data['factories'***REMOVED***[0***REMOVED***['object'***REMOVED***;
+             $this->arrayToFile($this->getModule()->getConfigExtFolder().'/servicemanager.config.php', $this->serviceManagerFile);
+             return;
+        }
+
+    }
+
     public function mergeServiceManagerConfig()
     {
+        $this->serviceManagerFile = require $this->getModule()->getConfigExtFolder().'/servicemanager.config.php';
+
+        if ($this->src !== null) {
+            $this->triggerMergeServiceManager();
+        }
+
+        if ($this->db !== null) {
+            $srcs = $this->getGearSchema()->getAllSrcByDb($this->db);
+
+            foreach ($srcs as $src) {
+                $this->src = $src;
+                $this->triggerMergeServiceManager();
+            }
+        }
+
+        return;
+
+
+
         $srcs = $this->getGearSchema()->__extract('src');
 
         $controllers = [***REMOVED***;
