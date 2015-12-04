710d4a0badce3d9d30ddd9db1a1bff772885406d
Mauricio Piber <mauriciopiber@gmail.com>
2015-12-04 21:27:51 +0000

    Atualizando

--- config/module.config.php
+++ config/module.config.php
@@ -15,7 +15,7 @@ return array(
     ),
     'speciality' => $speciality,
     'service_manager' => $serviceManager,
-    'gear' => array('version' => '0.1.131', 'acl' => true, 'name' => __NAMESPACE__),
+    'gear' => array('version' => '0.1.132', 'acl' => true, 'name' => __NAMESPACE__),
     'console' => $consoleRoutes,
     'controllers' => array(
         'invokables' => array(
