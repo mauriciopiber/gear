0e9d6588e0617903c76c0198247ac3562c72ae42
Mauricio Piber <mauriciopiber@gmail.com>
2015-12-07 08:02:44 +0000

    Adicionado modulo gear-json ao require

--- delete-tags-0.1.sh
+++ delete-tags-0.1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 sudo git ls-remote --tags origin | awk '/^(.*)(\s+)(.*0\.1\.[0-9***REMOVED****)$/ {print ":" $2}' | sudo xargs git push origin
-sudo git tag | awk '/^(.*0\.1\.[0-9***REMOVED****)$/ {print $1}' | sudo git tag -d
+sudo git tag | awk '/^(.*0\.1\.[0-9***REMOVED****)$/ {print $1}' | sudo xargs git tag -d
 
