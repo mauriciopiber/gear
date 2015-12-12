ffa4c089cfc9699f66b4e0b2be3a49e2b72fc354
Mauricio Piber <mauriciopiber@gmail.com>
2015-12-12 22:27:27 +0000

    Fix

--- config/module.config.php
+++ config/module.config.php
@@ -15,7 +15,7 @@ return array(
     ),
     'speciality' => $speciality,
     'service_manager' => $serviceManager,
-    'gear' => array('version' => '0.2.3', 'acl' => true, 'name' => __NAMESPACE__),
+    'gear' => array('version' => '0.2.4', 'acl' => true, 'name' => __NAMESPACE__),
     'console' => $consoleRoutes,
     'controllers' => array(
         'invokables' => array(
