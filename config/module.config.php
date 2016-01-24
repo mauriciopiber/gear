9ea5b19654f1b1b5c1efd0ff0903e6c5866e4d88
Mauricio Piber Fão <mauriciopiber@gmail.com>
2016-01-24 16:38:21 -0200

    Testar

--- config/module.config.php
+++ config/module.config.php
@@ -15,7 +15,7 @@ return array(
     ),
     'speciality' => $speciality,
     'service_manager' => $serviceManager,
-    'gear' => array('version' => '0.2.7', 'acl' => true, 'name' => __NAMESPACE__),
+    'gear' => array('version' => '0.2.8', 'acl' => true, 'name' => __NAMESPACE__),
     'console' => $consoleRoutes,
     'controllers' => array(
         'invokables' => array(
