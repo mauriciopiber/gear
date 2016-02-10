c6000bb247bb5cb098b2208854e6d9761392fa10
Mauricio Piber Fão <mauriciopiber@gmail.com>
2016-02-10 08:48:46 -0200

    dale

--- script/gear/AppService.sh
+++ script/gear/AppService.sh
@@ -3,8 +3,8 @@
 
 php public/index.php gear schema src delete Gear /var/www/gear-package ControllerService Service
 php public/index.php gear module src create Gear --name="ControllerService" --type=Service --namespace=Mvc\\View\\App
+php public/index.php gear module src create Gear --name="ServiceService" --type=Service --namespace=Mvc\\View\\App
 
-exit 1
 
-php public/index.php gear module src create Gear --name="ServiceService" --type=Service --namespace=Mvc\View\App
-php public/index.php gear module src create Gear --name="AppService" --type=Service --namespace=Constructor\Service --dependency="Mvc\View\App\ControllerService, Mvc\View\App\ServiceService"
+
+php public/index.php gear module src create Gear --name="AppService" --type=Service --namespace=Constructor\\Service --dependency="Mvc\View\App\ControllerService, Mvc\View\App\ServiceService"
