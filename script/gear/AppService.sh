9f27101e33a468fbd953a68778006d6164780857
Mauricio Piber Fão <mauriciopiber@gmail.com>
2016-02-10 06:14:43 -0200

    vamo de coração

--- script/gear/AppService.sh
+++ script/gear/AppService.sh
@@ -2,7 +2,7 @@
 
 
 php public/index.php gear schema src delete Gear /var/www/gear-package ControllerService Service
-php public/index.php gear module src create Gear --name="ControllerService" --type=Service --namespace=Mvc\View\App
+php public/index.php gear module src create Gear --name="ControllerService" --type=Service --namespace=Mvc\\View\\App
 
 exit 1
 
