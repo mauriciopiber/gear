f14d0cb884a7046238500e0a0fa2bb269e14e9b7
Mauricio Piber Fão <mauriciopiber@gmail.com>
2016-02-22 13:35:05 -0300

    Corrigido bug do upload de imagem

--- data/version/0.1.126/gear.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-base="/var/www/Gear"
-index="$base/public/index.php"
-
-php $index gear jenkins create job
-php $index gear jenkins update job
-php $index gear jenkins delete job
-php $index gear jenkins read job
