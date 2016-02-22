4ba60b95dc3b17360f5f4a2879beffb1c1c09c4a
Mauricio Piber Fão <mauriciopiber@gmail.com>
2016-02-22 18:46:09 -0300

    Modificado pasta Jenkins para Test, adicionado sniff para documentação e listeners para teste unitário

--- src/Gear/Module/TestService.php
+++ src/Gear/Module/TestService.php
@@ -15,7 +15,15 @@ class TestService extends AbstractMvc
     public function createTestsModuleAsProject()
     {
         $this->createBootstrapModuleAsProject();
-        $this->createAbstractFile();
+        $this->copyBuildXmlFile();
+        $this->copyphpdox();
+        $this->copyphpmd();
+        $this->copyphpunit();
+        $this->copyphpunitcoverage();
+        $this->copyphpunitfast();
+        $this->copyDocSniff();
+        $this->copyphpunitbenchmark();
+       // $this->createAbstractFile();
         return true;
     }
 
@@ -51,7 +59,113 @@ class TestService extends AbstractMvc
         );
     }
 
-    public function createAbstractFile()
+
+    public function copyphpdox()
+    {
+
+        $this->getFileCreator()->createFile(
+            'template/module/phpdox.xml.phtml',
+            array(
+                'module' => $this->str('url', $this->getModule()->getModuleName()),
+            ),
+            'phpdox.xml',
+            $this->getModule()->getMainFolder()
+        );
+    }
+
+
+    public function copyphpmd()
+    {
+
+        $this->getFileCreator()->createFile(
+            'template/module/test/phpmd.xml.phtml',
+            array(
+                'moduleName' => $this->str('label', $this->getModule()->getModuleName()),
+            ),
+            'phpmd.xml',
+            $this->getModule()->getTestFolder()
+        );
+
+
+    }
+
+    public function copyphpunitfast()
+    {
+        $this->getFileCreator()->createFile(
+            'template/module/test/phpunit-fast-coverage.xml.phtml',
+            array(
+                'moduleName' => $this->str('class', $this->getModule()->getModuleName()),
+            ),
+            'phpunit-fast-coverage.xml',
+            $this->getModule()->getTestFolder()
+        );
+    }
+
+    public function copyphpunitcoverage()
+    {
+        $this->getFileCreator()->createFile(
+            'template/module/test/phpunitci.xml.phtml',
+            array(
+                'moduleName' => $this->str('class', $this->getModule()->getModuleName()),
+            ),
+            'phpunitci.xml',
+            $this->getModule()->getTestFolder()
+        );
+
+    }
+
+    public function copyDocSniff()
+    {
+        $this->getFileCreator()->createFile(
+            'template/module/test/docs-phpcs.xml.phtml',
+            array(
+                'moduleName' => $this->str('class', $this->getModule()->getModuleName()),
+            ),
+            'docs-phpcs.xml',
+            $this->getModule()->getTestFolder()
+        );
+    }
+
+    public function copyphpunitbenchmark()
+    {
+        $this->getFileCreator()->createFile(
+            'template/module/test/phpunit-benchmark.xml.phtml',
+            array(
+                'moduleName' => $this->str('class', $this->getModule()->getModuleName()),
+            ),
+            'phpunit-benchmark.xml',
+            $this->getModule()->getTestFolder()
+        );
+    }
+
+
+
+    public function copyphpunit()
+    {
+        $this->getFileCreator()->createFile(
+            'template/module/test/phpunit.xml.phtml',
+            array(
+                'moduleName' => $this->str('class', $this->getModule()->getModuleName()),
+            ),
+            'phpunit.xml',
+            $this->getModule()->getTestFolder()
+        );
+    }
+
+
+    public function copyBuildXmlFile()
+    {
+        $this->getFileCreator()->createFile(
+            'template/module/build.xml.phtml',
+            array(
+                'moduleName' => $this->str('url', $this->getModule()->getModuleName()),
+            ),
+            'build.xml',
+            $this->getModule()->getMainFolder()
+        );
+    }
+
+   /*  public function createAbstractFile()
     {
 
         return $this->getFileCreator()->createFile(
@@ -62,5 +176,5 @@ class TestService extends AbstractMvc
             'AbstractTest.php',
             $this->getModule()->getTestUnitModuleFolder()
         );
-    }
+    } */
 }
