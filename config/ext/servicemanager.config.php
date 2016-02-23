06f8d89441a88be0c6fdfdb4ed0d9d8ef13b2e55
Mauricio Piber Fão <mauriciopiber@gmail.com>
2016-02-23 14:35:07 -0300

    Agora vou gerar toda questão específica do App

--- config/ext/servicemanager.config.php
+++ config/ext/servicemanager.config.php
@@ -1,8 +1,7 @@
 <?php return array (
   'invokables' =>
   array (
-    'Gear\Mvc\View\App\ControllerService' => 'Gear\Mvc\View\App\ControllerService',
-    'Gear\Mvc\View\App\ServiceService' => 'Gear\Mvc\View\App\ServiceService',
+
     'Gear\Util\Vector\ArrayService' => 'Gear\Util\Vector\ArrayService',
   ),
   'factories' =>
