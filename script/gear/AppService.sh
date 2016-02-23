c64c1d035b9318cfa672be8782975268ef0d3134
Mauricio Piber Fão <mauriciopiber@gmail.com>
2016-02-23 14:35:07 -0300

    Agora vou gerar toda questão específica do App

--- script/gear/AppService.sh
+++ script/gear/AppService.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
+php public/index.php gear module src create Gear --name="AppServiceSpecService" --extends="Mvc\\AbstractMvcTest"
+php public/index.php gear module src create Gear --name="AppControllerSpecService" --extends="Mvc\\AbstractMvcTest"  
 
-#php public/index.php gear schema src delete Gear /var/www/gear-package ControllerService Service
-#php public/index.php gear module src create Gear --name="ControllerService" --type=Service --namespace=Mvc\\View\\App
-php public/index.php gear module src create Gear --name="ServiceService" --type=Service --namespace=Mvc\\View\\App
+php public/index.php gear module src create Gear --name="AppServiceService" --extends="Mvc\\AbstractMvc" --dependency="Mvc\\View\\App\\AppServiceSpecService"
+php public/index.php gear module src create Gear --name="AppControllerService" --extends="Mvc\\AbstractMvc"  --dependency="Mvc\\View\\App\\AppControllerSpecService"
 
+php public/index.php gear module src create Gear --name="AppService" --type="Service" --namespace=Constructor\\App --dependency="Mvc\\View\\App\\AppServiceService,Mvc\\View\\App\\AppControllerService,\GearJson\Schema\SchemaService"
 
+php public/index.php gear module controler create Gear --name="AppController" --type=Console --namespace=Constructor\\App
+php public/index.php gear module activity create Gear AppController --name="create" --dependency="Constructor\\App\\AppService" 
+php public/index.php gear module activity create Gear AppController --name="delete"
 
-#php public/index.php gear module src create Gear --name="AppService" --type=Service --namespace=Constructor\\Service --dependency="Mvc\View\App\ControllerService, Mvc\View\App\ServiceService"
+ant unit
+exit 1
+
