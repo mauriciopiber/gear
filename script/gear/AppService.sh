3b57b1b392098851527cc7555e57d316a70eef4c
Mauricio Piber Fão <mauriciopiber@gmail.com>
2016-02-23 14:38:47 -0300

    Agora vou gerar toda questão específica do App

--- script/gear/AppService.sh
+++ script/gear/AppService.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-php public/index.php gear module src create Gear --name="AppServiceSpecService" --extends="Mvc\\AbstractMvcTest"
-php public/index.php gear module src create Gear --name="AppControllerSpecService" --extends="Mvc\\AbstractMvcTest"  
+php public/index.php gear module src create Gear --name="AppServiceSpecService" --type="Service" --extends="Mvc\\AbstractMvcTest" --namespace="Mvc\\View\\App"
 
-php public/index.php gear module src create Gear --name="AppServiceService" --extends="Mvc\\AbstractMvc" --dependency="Mvc\\View\\App\\AppServiceSpecService"
-php public/index.php gear module src create Gear --name="AppControllerService" --extends="Mvc\\AbstractMvc"  --dependency="Mvc\\View\\App\\AppControllerSpecService"
+ant
+exit 1
+
+php public/index.php gear module src create Gear --name="AppControllerSpecService" --extends="Mvc\\AbstractMvcTest"   --namespace="Mvc\\View\\App"
+
+php public/index.php gear module src create Gear --name="AppServiceService" --extends="Mvc\\AbstractMvc" --dependency="Mvc\\View\\App\\AppServiceSpecService"  --namespace="Mvc\\View\\App"
+php public/index.php gear module src create Gear --name="AppControllerService" --extends="Mvc\\AbstractMvc"  --dependency="Mvc\\View\\App\\AppControllerSpecService"  --namespace="Mvc\\View\\App"
 
 php public/index.php gear module src create Gear --name="AppService" --type="Service" --namespace=Constructor\\App --dependency="Mvc\\View\\App\\AppServiceService,Mvc\\View\\App\\AppControllerService,\GearJson\Schema\SchemaService"
 
@@ -12,6 +16,5 @@ php public/index.php gear module controler create Gear --name="AppController" --
 php public/index.php gear module activity create Gear AppController --name="create" --dependency="Constructor\\App\\AppService" 
 php public/index.php gear module activity create Gear AppController --name="delete"
 
-ant unit
-exit 1
+
 
