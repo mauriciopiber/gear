268f474f3f043b835291e7171770f699010ed190
Mauricio Piber Fão <mauriciopiber@gmail.com>
2016-03-01 05:51:01 -0300

    Commit

--- config/module.config.php
+++ config/module.config.php
@@ -51,7 +51,7 @@ return [
     'speciality' => $speciality,
     'service_manager' => $serviceManager,
     'gear' => [
-        'version' => '0.2.26',
+        'version' => '0.2.27',
         'acl' => true,
         'name' => __NAMESPACE__,
     ***REMOVED***,
