7199903c015b10f84ced8a5257ce11937f30bc72
Mauricio Piber Fão <mauriciopiber@gmail.com>
2016-03-14 15:31:14 -0300

    Voltei

--- config/module.config.php
+++ config/module.config.php
@@ -51,7 +51,7 @@ return [
     'speciality' => $speciality,
     'service_manager' => $serviceManager,
     'gear' => [
-        'version' => '0.3.0',
+        'version' => '0.2.31',
         'acl' => true,
         'name' => __NAMESPACE__,
     ***REMOVED***,
