cab30fcd100b9b360134a6ec39c38d1fbeac0cd3
Mauricio Piber Fão <mauriciopiber@gmail.com>
2016-04-07 17:48:48 -0300

    Atualizado

--- config/module.config.php
+++ config/module.config.php
@@ -51,7 +51,7 @@ return [
     'speciality' => $speciality,
     'service_manager' => $serviceManager,
     'gear' => [
-        'version' => '0.2.36',
+        'version' => '0.2.37',
         'acl' => true,
         'name' => __NAMESPACE__,
     ***REMOVED***,
