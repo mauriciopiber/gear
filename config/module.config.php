61206bd1ec0a0e160c3771f461bf3a7ac1684fd0
Mauricio Piber Fão <mauriciopiber@gmail.com>
2016-05-09 20:38:00 -0300

    Enviando

--- config/module.config.php
+++ config/module.config.php
@@ -51,7 +51,7 @@ return [
     'speciality' => $speciality,
     'service_manager' => $serviceManager,
     'gear' => [
-        'version' => '0.2.42',
+        'version' => '0.2.43',
         'acl' => true,
         'name' => __NAMESPACE__,
     ***REMOVED***,
