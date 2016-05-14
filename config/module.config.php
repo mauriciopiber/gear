927378edd473831b6c9f83530b6d814c65ca6607
Mauricio Piber Fão <mauriciopiber@gmail.com>
2016-05-14 07:06:30 -0300

    Teste

--- config/module.config.php
+++ config/module.config.php
@@ -51,7 +51,7 @@ return [
     'speciality' => $speciality,
     'service_manager' => $serviceManager,
     'gear' => [
-        'version' => '0.2.45',
+        'version' => '0.2.46',
         'acl' => true,
         'name' => __NAMESPACE__,
     ***REMOVED***,
