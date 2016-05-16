cf99e354456ded1211f36726979f2778cd5d52a3
Mauricio Piber Fão <mauriciopiber@gmail.com>
2016-05-16 14:09:50 -0300

    Corrigido

--- config/module.config.php
+++ config/module.config.php
@@ -51,7 +51,7 @@ return [
     'speciality' => $speciality,
     'service_manager' => $serviceManager,
     'gear' => [
-        'version' => '0.2.48',
+        'version' => '0.2.49',
         'acl' => true,
         'name' => __NAMESPACE__,
     ***REMOVED***,
