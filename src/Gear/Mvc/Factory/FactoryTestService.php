0d73ec601208c26ffd167fe6509a3aad52a69fef
Mauricio Piber Fão <mauriciopiber@gmail.com>
2016-06-02 12:01:39 -0300

    pre

---
+++ src/Gear/Mvc/Factory/FactoryTestService.php
@@ -0,0 +1,32 @@
+<?php
+namespace Gear\Mvc\Factory;
+
+use Gear\Mvc\AbstractMvcTest;
+use GearJson\Schema\SchemaServiceTrait;
+use Gear\Mvc\Config\ServiceManagerTrait;
+use GearJson\Src\Src;
+use GearJson\Controller\Controller;
+
+class FactoryTestService extends AbstractMvcTest
+{
+    public function createFactoryTest(Src $src, $location)
+    {
+        $name = $src->getName();
+
+        $trait = $this->getFileCreator();
+        $trait->setTemplate('template/module/mvc/factory/src-test.phtml');
+        $trait->setFileName($src->getName().'FactoryTest.php');
+        $trait->setLocation($location);
+        $trait->setOptions(
+            array(
+                //'piber' => 'test'
+                'module'    => $this->getModule()->getModuleName(),
+                'namespace' => $this->getCodeTest()->getNamespace($src),
+                'fullclass' => $this->getCodeTest()->getFullClassName($src),
+                'class' => $this->str('class', $name),
+            )
+        );
+
+        return $trait->render();
+    }
+}
