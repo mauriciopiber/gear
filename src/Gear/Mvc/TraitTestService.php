0eb51e20bca99d6e7eb04f4d75da9a5f043da568
Mauricio Piber Fão <mauriciopiber@gmail.com>
2016-06-02 11:31:40 -0300

    Criar teste da trait

--- src/Gear/Mvc/TraitTestService.php
+++ src/Gear/Mvc/TraitTestService.php
@@ -1,20 +1,22 @@
 <?php
 namespace Gear\Mvc;
 
-use Gear\Mvc\AbstractMvc;
+use Gear\Mvc\AbstractMvcTest;
 use GearJson\Src\Src;
 
-class TraitTestService extends AbstractMvc
+class TraitTestService extends AbstractMvcTest
 {
-    public function __construct($module, $fileCreator, $string)
+    public function __construct($module, $fileCreator, $string, $codeTest)
     {
         $this->module = $module;
         $this->fileCreator = $fileCreator;
         $this->stringService = $string;
+        $this->codeTest = $codeTest;
     }
 
     public function createTraitTest(Src $src, $location)
     {
+        $name = $src->getName();
 
         $trait = $this->getFileCreator();
         $trait->setTemplate('template/module/mvc/trait/src-test.phtml');
@@ -22,96 +24,14 @@ class TraitTestService extends AbstractMvc
         $trait->setLocation($location);
         $trait->setOptions(
             array(
-                'test' => 'piber'
-                //'module' => $this->getModule()->getModuleName(),
-                //'namespace' => $this->getCode()->getNamespace($src),
-                //'class' => $this->str('class', $name),
-                //'var'   => $this->str('var', $name),
-                //'lenght' => $this->str('var-lenght', $name),
-                //'srcType' => $src->getType(),
-                //'srcName' => $src->getName(),
-                //'service' => $service,
-                //'callable' => $callable
+                //'test' => 'piber'
+                'module'    => $this->getModule()->getModuleName(),
+                'namespace' => $this->getCodeTest()->getNamespace($src),
+                'fullclass' => $this->getCodeTest()->getFullClassName($src),
+                'class' => $this->str('class', $name),
             )
         );
 
         return $trait->render();
     }
-
-
-    /*
-    public function createTrait(
-        $src,
-        $location,
-        $name = null,
-        $testLocation = null,
-        $isSearchForm = false,
-        $specialName = null
-        ) {
-            if ($name === null) {
-                $name = $src->getName();
-            }
-
-
-
-            $trait = $this->getFileCreator();
-            $trait->setTemplate('template/module/mvc/trait/src.phtml');
-            $trait->setFileName($name.'Trait.php');
-            $trait->setLocation($location);
-
-            if ($testLocation !== null && is_dir($testLocation)) {
-
-                if ($isSearchForm) {
-                    $specialName = sprintf(
-                        '%s\Form\Search\%s',
-                        $this->getModule()->getModuleName(),
-                        $specialName
-                        );
-                } else {
-                    $specialName = sprintf(
-                        '%s\%s\%s',
-                        $this->getModule()->getModuleName(),
-                        $src->getType(),
-                        $src->getName()
-                        );
-                }
-
-                $traitTest = $this->getFileCreator();
-                $traitTest->setTemplate('template/test/trait.phtml');
-                $traitTest->setFileName($name.'TraitTest.php');
-                $traitTest->setLocation($testLocation);
-                $traitTest->setOptions(array(
-                    'className' => $name.'Trait',
-                    'class' => $name,
-                    'var' => $this->str('var-lenght', $name),
-                    'expected' => $specialName,
-                    'module' => $this->getModule()->getModuleName()
-                ));
-                $traitTest->render();
-
-
-            }
-
-            $callable = $this->getServiceManager()->getServiceCallable($src);
-
-
-            $service = $this->getServiceManager()->getServiceName($src);
-
-
-            $trait->setOptions(
-                array(
-                    'module' => $this->getModule()->getModuleName(),
-                    'namespace' => $this->getCode()->getNamespace($src),
-                    'class' => $this->str('class', $name),
-                    'var'   => $this->str('var', $name),
-                    'lenght' => $this->str('var-lenght', $name),
-                    'srcType' => $src->getType(),
-                    'srcName' => $src->getName(),
-                    'service' => $service,
-                    'callable' => $callable
-                )
-                );
-
-            return $trait->render();
-    }*/
 }
