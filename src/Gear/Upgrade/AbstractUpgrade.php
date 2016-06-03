d4b31e92b261847701eaa15072283524e114344a
Mauricio Piber Fão <mauriciopiber@gmail.com>
2016-06-03 16:50:54 -0300

    vamo dale

--- src/Gear/Upgrade/AbstractUpgrade.php
+++ src/Gear/Upgrade/AbstractUpgrade.php
@@ -20,6 +20,55 @@ abstract class AbstractUpgrade extends AbstractJsonService
 
     use AntUpgradeTrait;
 
+    /**
+     * Responsável por mostrar as mensagens de erro conforme vão aparecendo.
+     *
+     * @param string $message Mensagem do Erro.
+     * @return void
+     */
+
+    public function showError($message)
+    {
+        $this->console->writeLine($message, 0, 2);
+    }
+
+    /**
+     * Responsável por mostrar as mensagens de sucesso conforme vão aparecendo
+     *
+     * @param string $message Mensagem de sucesso.
+     * @return void
+     */
+    public function showCheck($message)
+    {
+        $this->console->writeLine($message, 0, 3);
+    }
+
+    /**
+     * Responsável por exibir todas mensagens após a conclusão do teste.
+     */
+    public function printUpgrades()
+    {
+        $count = count($this->errors);
+
+        $errors = ($count==1) ? 'Realizado %s upgrade.' : 'Realizados %s upgrades.';
+
+        $this->showError(sprintf($errors, count($this->errors)));
+
+        foreach ($this->errors as $i => $item) {
+            $this->showError(($i+1).'° '.$item);
+        }
+    }
+
+    public function show()
+    {
+        if (count($this->errors)) {
+            $this->printUpgrades();
+            return;
+        }
+
+        $this->console->writeLine('O sistema está atualizado.', 0, 3);
+        return;
+    }
     //abstract public function upgradeModule();
 
     //abstract public function upgradeProject();
