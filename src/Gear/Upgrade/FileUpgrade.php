4db413b48960967b3e31366864141b429fb93455
Mauricio Piber Fão <mauriciopiber@gmail.com>
2016-06-05 03:27:35 -0300

    VAMO VE

--- src/Gear/Upgrade/FileUpgrade.php
+++ src/Gear/Upgrade/FileUpgrade.php
@@ -4,9 +4,31 @@ namespace Gear\Upgrade;
 use Zend\ServiceManager\ServiceLocatorAwareInterface;
 use Zend\ServiceManager\ServiceLocatorAwareTrait;
 use Gear\Service\AbstractJsonService;
+use Gear\Project\ProjectServiceTrait;
+use Gear\Module\ModuleServiceTrait;
+use Gear\Util\Console\ConsoleAwareTrait;
+use Gear\Util\Prompt\ConsolePromptTrait;
 
 class FileUpgrade extends AbstractJsonService implements ServiceLocatorAwareInterface
 {
+    use ConsolePromptTrait;
+
+    use ConsoleAwareTrait;
+
+    use ProjectServiceTrait;
+
+    use ModuleServiceTrait;
+
+    public function __construct($console, $consolePrompt, $moduleService, $projectService, $module = null)
+    {
+        $this->console = $console;
+        $this->moduleService = $moduleService;
+        $this->projectService = $projectService;
+        $this->module = $module;
+        $this->consolePrompt = $consolePrompt;
+    }
+    //use DirEdgeTrait;
+
     use ServiceLocatorAwareTrait;
 
     public function upgradeModule($type = 'web')
--- test/unit/GearTest/UpgradeTest/FileUpgradeFactoryTest.php
+++ test/unit/GearTest/UpgradeTest/FileUpgradeFactoryTest.php
@@ -13,6 +13,27 @@ class FileUpgradeFactoryTest extends AbstractTestCase
     {
         $this->serviceLocator    = $this->prophesize('Zend\ServiceManager\ServiceLocatorInterface');
 
+
+        $this->serviceLocator->get('console')
+          ->willReturn($this->prophesize('Zend\Console\Adapter\Posix')->reveal())
+          ->shouldBeCalled();
+
+        $this->serviceLocator->get('Gear\Util\Prompt\ConsolePrompt')
+          ->willReturn($this->prophesize('Gear\Util\Prompt\ConsolePrompt')->reveal())
+          ->shouldBeCalled();
+
+        $this->serviceLocator->get('moduleService')
+          ->willReturn($this->prophesize('Gear\Module\ModuleService')->reveal())
+          ->shouldBeCalled();
+
+        $this->serviceLocator->get('projectService')
+          ->willReturn($this->prophesize('Gear\Project\ProjectService')->reveal())
+          ->shouldBeCalled();
+
+        $this->serviceLocator->get('moduleStructure')
+          ->willReturn($this->prophesize('Gear\Module\BasicModuleStructure')->reveal())
+          ->shouldBeCalled();
+
         $factory = new \Gear\Upgrade\FileUpgradeFactory();
 
         $instance = $factory->createService($this->serviceLocator->reveal());
