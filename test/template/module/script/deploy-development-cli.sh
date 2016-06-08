147c2d4162a3bd1f6a88b6b427a9a1ed95880692
Mauricio Piber Fão <mauriciopiber@gmail.com>
2016-06-08 02:28:21 -0300

    Atualizaçao do gear-development

--- test/template/module/script/deploy-development-cli.sh
+++ test/template/module/script/deploy-development-cli.sh
@@ -6,4 +6,10 @@ if [ "$?" != "0" ***REMOVED***; then
     composer update
 fi;
 
+database=$(php -r '$global = require_once("config/autoload/global.php"); echo $global["doctrine"***REMOVED***["connection"***REMOVED***["orm_default"***REMOVED***["params"***REMOVED***["dbname"***REMOVED***;')
+username=$(php -r '$local = require_once("config/autoload/local.php"); echo $local["doctrine"***REMOVED***["connection"***REMOVED***["orm_default"***REMOVED***["params"***REMOVED***["user"***REMOVED***;')
+password=$(php -r '$local = require_once("config/autoload/local.php"); echo $local["doctrine"***REMOVED***["connection"***REMOVED***["orm_default"***REMOVED***["params"***REMOVED***["password"***REMOVED***;')
+
+vendor/bin/database $database $username $password
+
 php public/index.php gear module dump-autoload GearIt
