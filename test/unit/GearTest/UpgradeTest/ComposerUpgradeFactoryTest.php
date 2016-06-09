6a9944cc7d11b927aa14906c8284e964bffb476f
Mauricio Piber Fão <mauriciopiber@gmail.com>
2016-06-09 05:56:52 -0300

    Adicionado upgrade do composer ao projeto

--- test/unit/GearTest/UpgradeTest/ComposerUpgradeFactoryTest.php
+++ test/unit/GearTest/UpgradeTest/ComposerUpgradeFactoryTest.php
@@ -16,6 +16,26 @@ class ComposerUpgradeFactoryTest extends AbstractTestCase
 
         $factory = new \Gear\Upgrade\ComposerUpgradeFactory();
 
+        $consolePrompt = $this->prophesize('Gear\Util\Prompt\ConsolePrompt');
+
+        $this->serviceLocator->get('Gear\Util\Prompt\ConsolePrompt')
+        ->willReturn($consolePrompt->reveal())
+        ->shouldBeCalled();
+
+        $this->serviceLocator->get('Gear\Edge\ComposerEdge')
+        ->willReturn($this->prophesize('Gear\Edge\ComposerEdge'))
+        ->shouldBeCalled();
+
+
+        $this->serviceLocator->get('config')->willReturn([***REMOVED***)->shouldBeCalled();
+
+
+        $module = $this->prophesize('Gear\Module\BasicModuleStructure');
+
+        $this->serviceLocator->get('moduleStructure')->willReturn($module->reveal())->shouldBeCalled();
+
+
+
         $instance = $factory->createService($this->serviceLocator->reveal());
 
         $this->assertInstanceOf('Gear\Upgrade\ComposerUpgrade', $instance);
