8caa47c35aaf916c1f0d6a510feee3881ed61272
Mauricio Piber Fão <mauriciopiber@gmail.com>
2016-06-10 18:17:51 -0300

    Adicionado esqueleto de Docs, mas precisa resolver issue de adicionar MD ao gear base util file

--- src/Gear/Module/Docs/Docs.php
+++ src/Gear/Module/Docs/Docs.php
@@ -6,23 +6,71 @@ use Gear\Service\AbstractJsonService;
 class Docs extends AbstractJsonService
 {
 
-    public function __construct()
+    public function __construct($module, $string, $file)
     {
-
+        $this->module = $module;
+        $this->stringService = $string;
+        $this->fileCreator = $file;
     }
 
-    public function createIndex()
+    public function createIndex($type = 'web')
     {
-        return true;
+        $version = '';
+
+        $config = [
+            'label' => $this->getModule()->getModuleName(),
+            'version' => $version
+        ***REMOVED***;
+
+        $location = $this->getModule()->getDocsFolder();
+
+        $file = $this->getFileCreator();
+        $file->setTemplate(sprintf('template/module/docs/index-%s.phtml', $type));
+        $file->setOptions($config);
+        $file->setLocation($location);
+        $file->setFileName('index.md');
+
+        return $file->render();
+
     }
 
     public function createConfig()
     {
-        return true;
+        $version = '';
+
+        $config = [
+            'label' => $this->getModule()->getModuleName(),
+            'version' => $version
+        ***REMOVED***;
+
+        $location = $this->getModule()->getMainFolder();
+
+        $file = $this->getFileCreator();
+        $file->setTemplate('template/module/docs/mkdocs.phtml');
+        $file->setOptions($config);
+        $file->setLocation($location);
+        $file->setFileName('mkdocs.yml');
+
+        return $file->render();
     }
 
     public function createReadme()
     {
-        return true;
+        $version = '';
+
+        $config = [
+            'label' => $this->getModule()->getModuleName(),
+            'version' => $version
+        ***REMOVED***;
+
+        $location = $this->getModule()->getMainFolder();
+
+        $file = $this->getFileCreator();
+        $file->setTemplate('template/module/docs/readme.phtml');
+        $file->setOptions($config);
+        $file->setLocation($location);
+        $file->setFileName('README.md');
+
+        return $file->render();
     }
 }
--- test/unit/GearTest/ModuleTest/DocsTest/DocsTest.php
+++ test/unit/GearTest/ModuleTest/DocsTest/DocsTest.php
@@ -2,6 +2,8 @@
 namespace GearTest\ModuleTest\DocsTest;
 
 use GearBaseTest\AbstractTestCase;
+use org\bovigo\vfs\vfsStream;
+use org\bovigo\vfs\vfsStreamWrapper;
 
 /**
  * @group Service
@@ -12,24 +14,85 @@ class DocsTest extends AbstractTestCase
     public function setUp()
     {
         parent::setUp();
-        $this->docs = new \Gear\Module\Docs\Docs();
+
+        vfsStream::setup('module');
+
+
+        $template       = new \Gear\Creator\TemplateService();
+        $template->setRenderer($this->mockPhpRenderer((new \Gear\Module)->getLocation().'/../../view'));
+
+        $fileService    = new \GearBase\Util\File\FileService();
+        $this->fileCreator    = new \Gear\Creator\File($fileService, $template);
+
+        $this->module = $this->prophesize('Gear\Module\BasicModuleStructure');
+
+        $this->string = new \GearBase\Util\String\StringService();
+
+        $this->template = (new \Gear\Module())->getLocation().'/../../test/template/module/docs';
     }
 
     public function testCreateIndexDocs()
     {
+        vfsStream::newDirectory('docs')->at(vfsStreamWrapper::getRoot());
+
+        $this->module->getModuleName()->willReturn('MyModule')->shouldBeCalled();
+        $this->module->getDocsFolder()->willReturn(vfsStream::url('module/docs'))->shouldBeCalled();
+
+        $this->docs = new \Gear\Module\Docs\Docs(
+            $this->module->reveal(),
+            $this->string,
+            $this->fileCreator
+        );
+
         $result = $this->docs->createIndex();
-        $this->assertTrue($result);
+
+        $expected = $this->template.'/index-web.phtml';
+
+        $this->assertEquals(
+            file_get_contents($expected),
+            file_get_contents($result)
+        );
     }
 
     public function testCreateConfig()
     {
+        $this->module->getModuleName()->willReturn('MyModule')->shouldBeCalled();
+        $this->module->getMainFolder()->willReturn(vfsStream::url('module'))->shouldBeCalled();
+
+        $this->docs = new \Gear\Module\Docs\Docs(
+            $this->module->reveal(),
+            $this->string,
+            $this->fileCreator
+        );
+
         $result = $this->docs->createConfig();
-        $this->assertTrue($result);
+
+        $expected = $this->template.'/mkdocs.phtml';
+
+        $this->assertEquals(
+            file_get_contents($expected),
+            file_get_contents($result)
+        );
     }
 
     public function testCreateReadme()
     {
+        $this->module->getModuleName()->willReturn('MyModule')->shouldBeCalled();
+        $this->module->getMainFolder()->willReturn(vfsStream::url('module'))->shouldBeCalled();
+
+        $this->docs = new \Gear\Module\Docs\Docs(
+            $this->module->reveal(),
+            $this->string,
+            $this->fileCreator
+        );
+
         $result = $this->docs->createReadme();
-        $this->assertTrue($result);
+
+        $expected = $this->template.'/readme.phtml';
+
+        $this->assertEquals(
+            file_get_contents($expected),
+            file_get_contents($result)
+        );
     }
 }
