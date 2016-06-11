3e65fccfec5a5683d816740c7cad7c78ab3d646c
Mauricio Piber Fão <mauriciopiber@gmail.com>
2016-06-11 05:06:42 -0300

    Finalizado início mínimo básico dos docs

--- src/Gear/Project/Docs/Docs.php
+++ src/Gear/Project/Docs/Docs.php
@@ -2,26 +2,77 @@
 namespace Gear\Project\Docs;
 
 use Gear\Service\AbstractJsonService;
+use Gear\Project\ProjectLocationTrait;
 
 class Docs extends AbstractJsonService
 {
-    public function __construct()
-    {
+    use ProjectLocationTrait;
 
+    public $config;
+
+    public function __construct($config, $string, $file)
+    {
+        $this->config = $config;
+        $this->stringService = $string;
+        $this->fileCreator = $file;
     }
 
-    public function createIndex()
+    public function createIndex($type = 'web')
     {
-        return true;
+
+        $config = [
+            'label' => $this->str('label', $this->config['gear'***REMOVED***['project'***REMOVED***['name'***REMOVED***),
+        ***REMOVED***;
+
+        $location = $this->getProject();
+
+        $file = $this->getFileCreator();
+        $file->setTemplate(sprintf('template/project/docs/index.phtml', $type));
+        $file->setOptions($config);
+        $file->setLocation($location);
+        $file->setFileName('index.md');
+
+        return $file->render();
+
     }
 
     public function createConfig()
     {
-        return true;
+        $version = '';
+
+        $config = [
+            'label' => $this->str('label', $this->config['gear'***REMOVED***['project'***REMOVED***['name'***REMOVED***),
+            //'version' => $version
+        ***REMOVED***;
+
+        $location = $this->getProject();
+
+        $file = $this->getFileCreator();
+        $file->setTemplate('template/project/docs/mkdocs.phtml');
+        $file->setOptions($config);
+        $file->setLocation($location);
+        $file->setFileName('mkdocs.yml');
+
+        return $file->render();
     }
 
     public function createReadme()
     {
-        return true;
+        //$version = '';
+
+        $config = [
+            'label' => $this->str('label', $this->config['gear'***REMOVED***['project'***REMOVED***['name'***REMOVED***),
+            //'version' => $version
+        ***REMOVED***;
+
+        $location = $this->getProject();
+
+        $file = $this->getFileCreator();
+        $file->setTemplate('template/project/docs/readme.phtml');
+        $file->setOptions($config);
+        $file->setLocation($location);
+        $file->setFileName('README.md');
+
+        return $file->render();
     }
 }
--- test/unit/GearTest/ProjectTest/DocsTest/DocsTest.php
+++ test/unit/GearTest/ProjectTest/DocsTest/DocsTest.php
@@ -3,6 +3,8 @@ namespace GearTest\ProjectTest\DocsTest;
 
 use GearBaseTest\AbstractTestCase;
 use Gear\Project\Docs\DocsTrait;
+use org\bovigo\vfs\vfsStream;
+use org\bovigo\vfs\vfsStreamWrapper;
 
 /**
  * @group Service
@@ -15,24 +17,88 @@ class DocsTest extends AbstractTestCase
     public function setUp()
     {
         parent::setUp();
-        $this->docs = new \Gear\Project\Docs\Docs();
+
+        vfsStream::setup('project');
+
+
+        $template       = new \Gear\Creator\TemplateService();
+        $template->setRenderer($this->mockPhpRenderer((new \Gear\Module)->getLocation().'/../../view'));
+
+        $fileService    = new \GearBase\Util\File\FileService();
+        $this->fileCreator    = new \Gear\Creator\File($fileService, $template);
+
+        $this->config = [
+            'gear' => [
+                'project' => [
+                    'name' => 'MyProject'
+                ***REMOVED***
+            ***REMOVED***
+        ***REMOVED***;
+
+        $this->string = new \GearBase\Util\String\StringService();
+
+        $this->template = (new \Gear\Module())->getLocation().'/../../test/template/project/docs';
     }
 
     public function testCreateIndexDocs()
     {
+        vfsStream::newDirectory('docs')->at(vfsStreamWrapper::getRoot());
+
+        $this->docs = new \Gear\Project\Docs\Docs(
+            $this->config,
+            $this->string,
+            $this->fileCreator
+        );
+
+        $this->docs->setProject(vfsStream::url('project'));
+
         $result = $this->docs->createIndex();
-        $this->assertTrue($result);
+
+        $expected = $this->template.'/index.phtml';
+
+        $this->assertEquals(
+            file_get_contents($expected),
+            file_get_contents($result)
+        );
     }
 
     public function testCreateConfig()
     {
+        $this->docs = new \Gear\Project\Docs\Docs(
+            $this->config,
+            $this->string,
+            $this->fileCreator
+        );
+
+        $this->docs->setProject(vfsStream::url('project'));
+
         $result = $this->docs->createConfig();
-        $this->assertTrue($result);
+
+        $expected = $this->template.'/mkdocs.phtml';
+
+        $this->assertEquals(
+            file_get_contents($expected),
+            file_get_contents($result)
+        );
     }
 
     public function testCreateReadme()
     {
+        $this->docs = new \Gear\Project\Docs\Docs(
+            $this->config,
+            $this->string,
+            $this->fileCreator
+        );
+
+        $this->docs->setProject(vfsStream::url('project'));
+
         $result = $this->docs->createReadme();
-        $this->assertTrue($result);
+
+        $expected = $this->template.'/readme.phtml';
+
+        $this->assertEquals(
+            file_get_contents($expected),
+            file_get_contents($result)
+        );
     }
 }
