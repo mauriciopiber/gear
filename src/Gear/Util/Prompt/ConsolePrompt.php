2040c98d40ede21ef873eab78e7a76646f80f951
Mauricio Piber Fão <mauriciopiber@gmail.com>
2016-06-12 00:38:01 -0300

    FORCE OK

--- src/Gear/Util/Prompt/ConsolePrompt.php
+++ src/Gear/Util/Prompt/ConsolePrompt.php
@@ -5,8 +5,19 @@ use Zend\Console\Prompt;
 
 class ConsolePrompt
 {
+    public $force;
+
+    public function __construct($force)
+    {
+        $this->force = $force;
+    }
+
     public function show($message)
     {
+        if ($this->force === true) {
+            return true;
+        }
+
         $confirm = new Prompt\Confirm($message);
         $result = $confirm->show();
         return $result;
