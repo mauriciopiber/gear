203f3906acee396d94979ba3fac19e2e1ec58820
Mauricio Piber Fão <mauriciopiber@gmail.com>
2016-06-13 18:16:36 -0300

    Será

--- config/module.config.php
+++ config/module.config.php
@@ -51,7 +51,7 @@ return [
     'speciality' => $speciality,
     'service_manager' => $serviceManager,
     'gear' => [
-        'version' => '0.2.61',
+        'version' => '0.2.62',
         'acl' => true,
         'name' => __NAMESPACE__,
     ***REMOVED***,
