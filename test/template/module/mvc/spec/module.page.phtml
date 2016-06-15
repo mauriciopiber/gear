bf4617d7abc3ad67a029a58ca9ffcae9fd74bf70
Mauricio Piber Fão <mauriciopiber@gmail.com>
2016-06-15 11:05:28 -0300

    Adicionar feature em gearbase

---
+++ test/template/module/mvc/spec/module.page.phtml
@@ -0,0 +1,26 @@
+'use strict';
+
+module.exports = {
+    get: function() {
+        return browser.get('/my-module');
+    },
+    pageHeader: function() {
+        return element(by.css('.page-header')).getText();
+    },
+    pageTitle: function() {
+        return browser.getTitle();
+    },
+    breadscrumb: function() {
+        return {
+            first: function() {
+                return element(by.css('.breadcrumb li:nth-child(1) > a'));
+            },
+            second: function() {
+                return element(by.css('.breadcrumb li:nth-child(2) > a'));
+            },
+            third: function() {
+                return element(by.css('.breadcrumb li:nth-child(3)'));
+            }
+        }
+    }
+};
--- test/unit/GearTest/MvcTest/SpecTest/FeatureTest/FeatureTest.php
+++ test/unit/GearTest/MvcTest/SpecTest/FeatureTest/FeatureTest.php
@@ -2,45 +2,62 @@
 namespace GearTest\MvcTest\SpecTest\FeatureTest;
 
 use GearBaseTest\AbstractTestCase;
-use Gear\Mvc\Spec\Feature\FeatureTrait;
+use org\bovigo\vfs\vfsStream;
+use org\bovigo\vfs\vfsStreamWrapper;
 
 /**
- * @group Service
+ * @group Spec
  */
 class FeatureTest extends AbstractTestCase
 {
-    use FeatureTrait;
 
-    /**
-     * @group Gear
-     * @group Feature
-     */
-    public function testServiceLocator()
+    public function setUp()
     {
-        $serviceLocator = $this->getFeature()->getServiceLocator();
-        $this->assertInstanceOf('Zend\ServiceManager\ServiceManager', $serviceLocator);
+        parent::setUp();
+        vfsStream::setup('module');
+        vfsStream::newDirectory('public')->at(vfsStreamWrapper::getRoot());
+        vfsStream::newDirectory('public/js')->at(vfsStreamWrapper::getRoot());
+        vfsStream::newDirectory('public/js/spec')->at(vfsStreamWrapper::getRoot());
+        vfsStream::newDirectory('public/js/spec/e2e')->at(vfsStreamWrapper::getRoot());
+        vfsStream::newDirectory('public/js/spec/e2e/index')->at(vfsStreamWrapper::getRoot());
+
+        $this->assertFileExists('vfs://module/public/js/spec/e2e/index');
+
+        $this->module = $this->prophesize('Gear\Module\BasicModuleStructure');
+        $this->module->getPublicJsSpecEndFolder()
+          ->willReturn(vfsStream::url('module/public/js/spec/e2e'))
+          ->shouldBeCalled();
+
+        $this->module->getModuleName()->willReturn('MyModule')->shouldBeCalled();
+
+        $this->string = new \GearBase\Util\String\StringService();
+
+        $template       = new \Gear\Creator\TemplateService();
+        $template->setRenderer($this->mockPhpRenderer((new \Gear\Module)->getLocation().'/../../view'));
+
+        $fileService    = new \GearBase\Util\File\FileService();
+        $this->fileCreator    = new \Gear\Creator\File($fileService, $template);
+
+        $this->template = (new \Gear\Module())->getLocation().'/../../test/template/module/mvc/spec';
     }
 
-    /**
-     * @group Gear
-     * @group Feature
-    */
-    public function testGet()
+    public function testCreateIndexFeature()
     {
-        $feature = $this->getFeature();
-        $this->assertInstanceOf('Gear\Mvc\Spec\Feature\Feature', $feature);
-    }
 
-    /**
-     * @group Gear
-     * @group Feature
-    */
-    public function testSet()
-    {
-        $mockFeature = $this->getMockSingleClass(
-            'Gear\Mvc\Spec\Feature\Feature'
+        $feature = new \Gear\Mvc\Spec\Feature\Feature();
+        $feature->setModule($this->module->reveal());
+        $feature->setStringService($this->string);
+        $feature->setFileCreator($this->fileCreator);
+
+
+        $file = $feature->createIndexFeature();
+
+        $expected = $this->template.'/module.feature.phtml';
+
+        $this->assertEquals(
+            file_get_contents($expected),
+            file_get_contents($file)
         );
-        $this->setFeature($mockFeature);
-        $this->assertEquals($mockFeature, $this->getFeature());
+
     }
 }
