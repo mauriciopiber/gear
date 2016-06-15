d45d0f12901cf87554cb319d9ce3ca1c09b0cd7e
Mauricio Piber Fão <mauriciopiber@gmail.com>
2016-06-15 16:57:34 -0300

    Funcionando o primeiro Step automático, ainda há muita coisa para fazer até ficar bom

--- test/integration/module-as-project-create-web.sh
+++ test/integration/module-as-project-create-web.sh
@@ -9,8 +9,8 @@ modulepath="$base/my-module"
 rm -R /var/www/gear-package/my-module
 
 
-php public/index.php gear module-as-project create MyModule $base --type=web --force
+sudo php public/index.php gear module-as-project create MyModule $base --type=web --force
 
-cd $modulepath && script/deploy-development.sh
+cd $modulepath && sudo script/deploy-development.sh
 
 cd $modulepath && ant
