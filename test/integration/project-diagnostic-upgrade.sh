14c3035ad4c552711da4af8558992bc7b4746dc3
Mauricio Piber Fão <mauriciopiber@gmail.com>
2016-06-16 02:45:54 -0300

    Finalizado script de Diagnostic/Upgrade

--- test/integration/project-diagnostic-upgrade.sh
+++ test/integration/project-diagnostic-upgrade.sh
@@ -14,3 +14,39 @@ php public/index.php gear project create GearProject --database=$database --user
 ls -l $projectpath
 
 cd $projectpath && php public/index.php gear project diagnostic
+
+
+rm $projectpath/build.xml
+rm $projectpath/package.json
+rm $projectpath/composer.json
+
+
+rm -R $projectpath/node_modules
+rm -R $projectpath/script
+rm -R $projectpath/data/logs
+rm -R $projectpath/data/cache/configcache
+rm -R $projectpath/public/upload
+rm -R $projectpath/data/DoctrineModule/cache
+rm -R $projectpath/data/DoctrineORMModule/Proxy
+rm -R $projectpath/data/session
+rm -R $projectpath/build
+
+rm $projectpath/script/deploy-testing.sh
+rm $projectpath/script/deploy-development.sh
+rm $projectpath/script/deploy-production.sh
+rm $projectpath/script/deploy-staging.sh
+rm $projectpath/script/deploy-load.sh
+rm $projectpath/package.json
+rm $projectpath/gulpfile.js
+rm $projectpath/data/config.json
+rm $projectpath/mkdocs.yml
+rm $projectpath/docs/index.md
+rm $projectpath/README.md
+rm $projectpath/codeception.yml
+
+cd $projectpath && php public/index.php gear project diagnostic
+  
+cd $projectpath && php public/index.php gear project upgrade --force
+
+cd $projectpath && php public/index.php gear project diagnostic
+
