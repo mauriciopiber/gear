6af81605d398e9aef2856eeccf5e9dc8c93b93c9
Mauricio Piber Fão <mauriciopiber@gmail.com>
2016-06-16 01:36:09 -0300

    Está criando o controller

--- test/integration/project-diagnostic-upgrade.sh
+++ test/integration/project-diagnostic-upgrade.sh
@@ -11,4 +11,6 @@ projectpath="$basepath/$project"
 
 php public/index.php gear project create GearProject --database=$database --username=$username --password=$password --basepath=$basepath
 
+ls -l $projectpath
+
 cd $projectpath && php public/index.php gear project diagnostic
