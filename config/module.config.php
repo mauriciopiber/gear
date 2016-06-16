babab098902d96b441287e296b6c6ee513028b27
Mauricio Piber Fão <mauriciopiber@gmail.com>
2016-06-16 01:16:29 -0300

    Será

--- config/module.config.php
+++ config/module.config.php
@@ -53,7 +53,7 @@ return [
     'gear' => [
         'modules' => [
             'gear' => [
-                'version' => '0.2.65',
+                'version' => '0.2.66',
                 'acl' => true,
                 'name' => __NAMESPACE__,
             ***REMOVED***
