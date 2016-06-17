70f1d4a4d434574073cb1522ccbd6cf5283e26df
Mauricio Piber Fão <mauriciopiber@gmail.com>
2016-06-17 05:55:51 -0300

    Fazendo funcionar o maravilhoso Config, deletado Config antigo.

--- test/integration/project-module-create-web.sh
+++ test/integration/project-module-create-web.sh
@@ -1,16 +1,1 @@
 #!/bin/bash
-
-
-echo "Module As Project CLI"
-
-base="/var/www/gear-package"
-modulepath="$base/my-module"
-
-rm -R /var/www/gear-package/my-module
-
-
-sudo php public/index.php gear module create MyModule $base --type=web --force --as-project
-
-cd $modulepath && sudo script/deploy-development.sh
-
-cd $modulepath && ant
