7bb3b8ea1c9129c6f68c48aad13c53fd40ccdba5
Mauricio Piber Fão <mauriciopiber@gmail.com>
2016-06-20 19:26:24 -0300

    Alpha-2

--- config/module.config.php
+++ config/module.config.php
@@ -53,7 +53,7 @@ return [
     'gear' => [
         'modules' => [
             'gear' => [
-                'version' => '0.2.75',
+                'version' => '0.2.76',
                 'acl' => true,
                 'name' => __NAMESPACE__,
             ***REMOVED***
