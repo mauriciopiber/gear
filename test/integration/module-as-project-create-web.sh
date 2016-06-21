6c46513ace379d2eed08ef334a7b1fc2050c3a50
Mauricio Piber Fão <mauriciopiber@gmail.com>
2016-06-21 17:28:29 -0300

    vamo dale

--- test/integration/module-as-project-create-web.sh
+++ test/integration/module-as-project-create-web.sh
@@ -6,9 +6,9 @@ echo "Module As Project CLI"
 base="/var/www/gear-package"
 modulepath="$base/my-module"
 
-rm -R /var/www/gear-package/my-module
+#rm -R /var/www/gear-package/my-module
 
-
+sudo php public/index.php gear schema delete MyModule $base
 sudo php public/index.php gear module-as-project create MyModule $base --type=web --force
 
 cd $modulepath && sudo script/deploy-development.sh
