799978ca30add6626855f703a4c6343c5014c2da
Mauricio Piber Fão <mauriciopiber@gmail.com>
2016-06-29 00:46:58 -0300

    Fazendo uma breve análise

--- test/unit/GearTest/MvcTest/ControllerTest/ControllerServiceTest.php
+++ test/unit/GearTest/MvcTest/ControllerTest/ControllerServiceTest.php
@@ -24,7 +24,7 @@ class ControllerServiceTest extends AbstractTestCase
         $this->assertFileExists('vfs://module/src/MyModule/Controller');
 
         $this->module = $this->prophesize('Gear\Module\BasicModuleStructure');
-        $this->module->getControllerFolder()->willReturn(vfsStream::url('module/src/MyModule/Controller'))->shouldBeCalled();
+
         $this->module->getModuleName()->willReturn('MyModule')->shouldBeCalled();
 
         $this->string = new \GearBase\Util\String\StringService();
@@ -36,10 +36,56 @@ class ControllerServiceTest extends AbstractTestCase
         $this->fileCreator    = new \Gear\Creator\File($fileService, $template);
 
         $this->template = (new \Gear\Module())->getLocation().'/../../test/template/module/index-web';
+
+        $this->templates =  (new \Gear\Module())->getLocation().'/../../test/template/module/mvc/controller';
+
+
+        $this->code = $this->prophesize('Gear\Creator\Code');
+
+        $this->factoryService = $this->prophesize('Gear\Mvc\Factory\FactoryService');
     }
+
+    /**
+     * @group now
+     */
+    public function testCreateController()
+    {
+        $controller = new \GearJson\Controller\Controller([
+            'name' => 'MyController',
+            'object' => '%s\Controller\MyController',
+            'service' => 'factories'
+        ***REMOVED***);
+
+        $controllerService = new \Gear\Mvc\Controller\ControllerService();
+        $controllerService->setFileCreator($this->fileCreator);
+        $controllerService->setStringService($this->string);
+        $controllerService->setModule($this->module->reveal());
+
+        $this->code->getLocation($controller)->willReturn(vfsStream::url('module/src/MyModule/Controller'))->shouldBeCalled();
+        $this->code->getExtends($controller)->willReturn('AbstractActionController')->shouldBeCalled();
+        $this->code->getUse($controller)->willReturn('use Zend\Mvc\Controller\AbstractActionController;'.PHP_EOL)->shouldBeCalled();
+        $this->code->getNamespace($controller)->willReturn('MyModule\Controller')->shouldBeCalled();
+
+        $controllerService->setCode($this->code->reveal());
+
+        $this->factoryService->createFactory($controller, vfsStream::url('module/src/MyModule/Controller'))->shouldBeCalled();
+
+        $controllerService->setFactoryService($this->factoryService->reveal());
+
+        $file = $controllerService->buildController($controller);
+
+        $expected = $this->templates.'/CreateController.phtml';
+
+        $this->assertEquals(
+            file_get_contents($expected),
+            file_get_contents($file)
+        );
+    }
+
 
     public function testCreateModuleController()
     {
+        $this->module->getControllerFolder()->willReturn(vfsStream::url('module/src/MyModule/Controller'))->shouldBeCalled();
 
         $controller = new \Gear\Mvc\Controller\ControllerService();
         $controller->setFileCreator($this->fileCreator);
@@ -59,6 +105,8 @@ class ControllerServiceTest extends AbstractTestCase
 
     public function testCreateModuleControllerFactory()
     {
+        $this->module->getControllerFolder()->willReturn(vfsStream::url('module/src/MyModule/Controller'))->shouldBeCalled();
+
         $controller = new \Gear\Mvc\Controller\ControllerService();
         $controller->setFileCreator($this->fileCreator);
         $controller->setStringService($this->string);
