817aae0570bdddaaabb07c6f45900bc5aebab806
Mauricio Piber Fão <mauriciopiber@gmail.com>
2016-07-11 02:31:44 -0300

    Correção do nome das colunas realizada

--- test/integration/constructor-db-all/20160123222067_all_columns_db.php
+++ test/integration/constructor-db-all/20160123222067_all_columns_db.php
@@ -18,14 +18,14 @@ class AllColumnsDb extends AbstractMigration
                 'varchar_telephone',
                 'varchar_email'
             ***REMOVED***,
-            'boolean' => [
+            'tinyint' => [
                 'tinyint_tinyint',
                 'tinyint_checkbox'
             ***REMOVED***,
             'int' => [
                 'int_int',
                 'int_checkbox',
-                'int_foreign_key'
+                'id_int_foreign_key'
             ***REMOVED***,
             'time' => [
                 'time_time'
@@ -61,60 +61,70 @@ class AllColumnsDb extends AbstractMigration
      **/
     public function change()
     {
+        /**
         $table = $this->table('int_dep_three', ['id' => 'id_int_dep_three'***REMOVED***);
         $table->addColumn('dep_name', 'string', ['null' => false***REMOVED***);
         $table->create();
+        */
 
 
-        $table = $this->table('int_dep_four', ['id' => 'id_int_dep_four'***REMOVED***);
+        $table = $this->table('int_foreign_key', ['id' => 'id_int_foreign_key'***REMOVED***);
         $table->addColumn('dep_name', 'string', ['null' => false***REMOVED***);
         $table->create();
 
-        $columns = [
-            'all_columns',
-            'email',
-            'upload_image',
-            'url',
-            'telephone',
-            'unique_id',
-        ***REMOVED***;
-
-
         $table = $this->table('all_columns_db', ['id' => 'id_all_columns_db'***REMOVED***);
 
-        foreach ($columns as $column) {
-            $table->addColumn($column.'_one', 'string', ['null' => true, 'limit' => '255'***REMOVED***);
-            $table->addColumn($column.'_two', 'string', ['null' => true, 'limit' => '255'***REMOVED***);
+        $columns = $this->getColumnsNames();
+
+        foreach ($columns['string'***REMOVED*** as $columnName) {
+            $table->addColumn($columnName, 'string', ['null' => true, 'limit' => '255'***REMOVED***);
         }
 
+        foreach ($columns['date'***REMOVED*** as $columnName) {
+            $table->addColumn($columnName, 'date', ['null' => true***REMOVED***);
+        }
 
-        /* TABLE 1 */
+        foreach ($columns['datetime'***REMOVED*** as $columnName) {
+            $table->addColumn($columnName, 'datetime', ['null' => true***REMOVED***);
+        }
 
-        $table->addColumn('date_enus', 'date', ['null' => true***REMOVED***);
-        $table->addColumn('date_ptbr', 'date', ['null' => true***REMOVED***);
-        $table->addColumn('datetime_enus', 'datetime', ['null' => true***REMOVED***);
-        $table->addColumn('datetime_ptbr', 'datetime', ['null' => true***REMOVED***);
-        $table->addColumn('time_enus', 'time', ['null' => true***REMOVED***);
-        $table->addColumn('time_ptbr', 'time', ['null' => true***REMOVED***);
-        $table->addColumn('text_enus', 'text', ['null' => true***REMOVED***);
-        $table->addColumn('text_ptbr', 'text', ['null' => true***REMOVED***);
-        $table->addColumn('decimal_enus', 'decimal', ['null' => true, 'precision' => 10, 'scale' => 2***REMOVED***);
-        $table->addColumn('decimal_ptbr', 'decimal', ['null' => true, 'precision' => 10, 'scale' => 2***REMOVED***);
-        $table->addColumn('int_enus', 'integer', ['null' => true***REMOVED***);
-        $table->addColumn('int_ptbr', 'integer', ['null' => true***REMOVED***);
-        $table->addColumn('checkbox', 'integer', ['null' => true***REMOVED***);
-        $table->addColumn('tinyint_enus', 'boolean', ['null' => true***REMOVED***);
-        $table->addColumn('tinyint_ptbr', 'boolean', ['null' => true***REMOVED***);
-        $table->addColumn('all_columns_upload_one', 'string', ['null' => true, 'limit' => '255'***REMOVED***);
-        $table->addColumn('all_columns_upload_two', 'string', ['null' => true, 'limit' => '255'***REMOVED***);
-        $table->addColumn('id_int_dep_three', 'integer', ['null' => true***REMOVED***);
-        $table->addColumn('id_int_dep_four', 'integer', ['null' => true***REMOVED***);
-        $table->addForeignKey('id_int_dep_three', 'int_dep_three', 'id_int_dep_three', array('delete'=> 'CASCADE', 'update'=> 'CASCADE'));
-        $table->addForeignKey('id_int_dep_four', 'int_dep_four', 'id_int_dep_four', array('delete'=> 'CASCADE', 'update'=> 'CASCADE'));
+        foreach ($columns['time'***REMOVED*** as $columnName) {
+            $table->addColumn($columnName, 'time', ['null' => true***REMOVED***);
+        }
+
+        foreach ($columns['decimal'***REMOVED*** as $columnName) {
+            $table->addColumn($columnName, 'decimal', ['null' => true, 'precision' => 10, 'scale' => 2***REMOVED***);
+        }
+
+        foreach ($columns['int'***REMOVED*** as $columnName) {
+            $table->addColumn($columnName, 'integer', ['null' => true***REMOVED***);
+        }
+
+        foreach ($columns['tinyint'***REMOVED*** as $columnName) {
+            $table->addColumn($columnName, 'boolean', ['null' => true***REMOVED***);
+        }
+
+        foreach ($columns['text'***REMOVED*** as $columnName) {
+            $table->addColumn($columnName, 'text', ['null' => true***REMOVED***);
+        }
+
+        $table->addForeignKey('id_int_foreign_key', 'int_foreign_key', 'id_int_foreign_key', array('delete'=> 'CASCADE', 'update'=> 'CASCADE'));
+        //$table->addForeignKey('id_int_dep_four', 'int_dep_four', 'id_int_dep_four', array('delete'=> 'CASCADE', 'update'=> 'CASCADE'));
 
 
         $table->create();
 
+        $imageupload = $this->table('upload_image', ['id' => 'id_upload_image'***REMOVED***);
+        $imageupload->addColumn('id_all_columns_db', 'integer', ['null' => true***REMOVED***);
+        $imageupload->addForeignKey('id_all_columns_db', 'all_columns_db', 'id_all_columns_db', array('delete'=> 'CASCADE', 'update'=> 'CASCADE'));
+       // $imageupload->addColumn('id_all_columns_db_req', 'integer', ['null' => true***REMOVED***);
+       // $imageupload->addForeignKey('id_all_columns_db_req', 'all_columns_db_req', 'id_all_columns_db_req', array('delete'=> 'CASCADE', 'update'=> 'CASCADE'));
+       // $imageupload->addColumn('id_all_columns_db_mix', 'integer', ['null' => true***REMOVED***);
+       // $imageupload->addForeignKey('id_all_columns_db_mix', 'all_columns_db_mix', 'id_all_columns_db_mix', array('delete'=> 'CASCADE', 'update'=> 'CASCADE'));
+        $imageupload->update();
+
+
+        /**
 
         $columns[***REMOVED*** = 'password_verify';
 
@@ -125,7 +135,7 @@ class AllColumnsDb extends AbstractMigration
             $table->addColumn($column.'_two', 'string', ['null' => false, 'limit' => '255'***REMOVED***);
         }
 
-        /* TABLE 2 */
+
 
         $table->addColumn('date_enus', 'date', ['null' => false***REMOVED***);
         $table->addColumn('date_ptbr', 'date', ['null' => false***REMOVED***);
@@ -160,7 +170,7 @@ class AllColumnsDb extends AbstractMigration
             $table->addColumn($column.'_two', 'string', ['null' => false, 'limit' => '255'***REMOVED***);
         }
 
-        /* TABLE 3 */
+
         $table->addColumn('date_enus', 'date', ['null' => true***REMOVED***);
         $table->addColumn('date_ptbr', 'date', ['null' => true***REMOVED***);
         $table->addColumn('date_req_enus', 'date', ['null' => false***REMOVED***);
@@ -208,6 +218,7 @@ class AllColumnsDb extends AbstractMigration
        // $imageupload->addForeignKey('id_all_columns_db_mix', 'all_columns_db_mix', 'id_all_columns_db_mix', array('delete'=> 'CASCADE', 'update'=> 'CASCADE'));
         $imageupload->update();
 
+*/
 
     }
 }
--- test/integration/constructor-db-all/project-create.sh
+++ test/integration/constructor-db-all/project-create.sh
@@ -14,25 +14,24 @@ gearpath="$base/gear"
 sudo rm -R "$modulepath/src"
 sudo rm -R "$modulepath/test"
 
-###$modulepath/vendor/bin/database my_module root gear
+$modulepath/vendor/bin/database my_module root gear
 
 sudo php public/index.php gear schema delete MyModule $base
 sudo php public/index.php gear module-as-project create MyModule $base --type=web --force
 
 cd $modulepath && sudo script/deploy-development.sh
 
-
-####sudo cp "$fullpath/20160123222067_all_columns_db.php" $modulepath/data/migrations/
+sudo cp "$fullpath/20160123222067_all_columns_db.php" $modulepath/data/migrations/
 
 cd $modulepath && sudo vendor/bin/phinx migrate
 
-###cd $modulepath && sudo vendor/bin/unload-module BjyAuthorize
+cd $modulepath && sudo vendor/bin/unload-module BjyAuthorize
 
-###cd $modulepath && sudo php public/index.php gear database fix
+cd $modulepath && sudo php public/index.php gear database fix
 
 cd $gearpath && sudo php public/index.php gear module construct MyModule $base --file="$basedir/gear.yml"
 
-####cd $modulepath && sudo script/load.sh
+cd $modulepath && sudo script/load.sh
 
 #cd $modulepath && ant unit
 cd $modulepath && ant protractor-tag -Dtag="@all-columns-db-view"
