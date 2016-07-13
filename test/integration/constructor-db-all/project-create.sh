4828aed253cb1491504514c4ef70d91cf253566b
Mauricio Piber Fão <mauriciopiber@gmail.com>
2016-07-13 09:24:15 -0300

    Adicionando documentação

--- test/integration/constructor-db-all/project-create.sh
+++ test/integration/constructor-db-all/project-create.sh
@@ -41,8 +41,7 @@ cd $gearpath && sudo php public/index.php gear module construct MyModule $base -
 ###cd $modulepath && sudo script/load.sh
 
 cd $modulepath && ant unit
-cd $modulepath && ant protractor
-
+cd $modulepath && ant protractor-tag -Dtag="@all-columns-db"
 cd $modulepath && ant phpcs-docs
 
 #cd $modulepath && ant unit-group -Ds=Controller.Create
