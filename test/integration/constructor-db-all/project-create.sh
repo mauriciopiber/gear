d005a54a8bed8ea09db4d67ef1a49ac22778c413
Mauricio Piber Fão <mauriciopiber@gmail.com>
2016-07-16 08:16:05 -0300

    Piada.

--- test/integration/constructor-db-all/project-create.sh
+++ test/integration/constructor-db-all/project-create.sh
@@ -18,8 +18,8 @@ sudo rm -R "$modulepath/test"
 
 
 ### can be turned off
-###$modulepath/vendor/bin/database my_module root gear
-###sudo rm $modulepath/data/my-module.mysql.sql
+$modulepath/vendor/bin/database my_module root gear
+sudo rm $modulepath/data/my-module.mysql.sql
 
 sudo php public/index.php gear schema delete MyModule $base
 sudo php public/index.php gear module-as-project create MyModule $base --type=web --force
@@ -27,30 +27,31 @@ sudo php public/index.php gear module-as-project create MyModule $base --type=we
 cd $modulepath && sudo script/deploy-development.sh
 
 ### can be turned off
-###sudo cp "$fullpath/20160123222067_all_columns_db.php" $modulepath/data/migrations/
+sudo cp "$fullpath/20160123222067_all_columns_db.php" $modulepath/data/migrations/
 
 ### can be turned off
-###cd $modulepath && sudo vendor/bin/phinx migrate
+cd $modulepath && sudo vendor/bin/phinx migrate
 
 ### can be turned off
-###cd $modulepath && sudo vendor/bin/unload-module BjyAuthorize
+cd $modulepath && sudo vendor/bin/unload-module BjyAuthorize
 
 ### can be turned off
-###cd $modulepath && sudo php public/index.php gear database fix
+cd $modulepath && sudo php public/index.php gear database fix
 
 cd $gearpath && sudo php public/index.php gear module construct MyModule $base --file="$basedir/gear.yml"
 
 ### can be turned off
-###cd $modulepath && sudo script/load.sh
+cd $modulepath && sudo script/load.sh
+
+cd $modulepath && ant unit
+#cd $modulepath && ant protractor
+
 
 #cd $modulepath && ant unit
 ###cd $modulepath && ant protractor-tag -Dtag="@all-columns-db"
 ###cd $modulepath && ant phpcs-docs
 
 #cd $modulepath && ant unit-group -Ds=Controller.Create
-
-cd $modulepath && ant unit
-cd $modulepath && ant protractor
 
 
 #cd $modulepath && ant protractor-tag -Dtag="@all-columns-db-create"
