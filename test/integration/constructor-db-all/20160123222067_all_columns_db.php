4b964146710331aabb4928a16d37011a2bb48617
Mauricio Piber Fão <mauriciopiber@gmail.com>
2016-07-20 01:58:28 -0300

    Tá, chega

--- test/integration/constructor-db-all/20160123222067_all_columns_db.php
+++ test/integration/constructor-db-all/20160123222067_all_columns_db.php
@@ -56,7 +56,7 @@ class AllColumnsDb extends AbstractMigration
         $table = $this->table('all_columns_db', ['id' => 'id_all_columns_db'***REMOVED***);
 
         foreach ($columns['string'***REMOVED*** as $columnName) {
-            $table->addColumn($columnName, 'string', ['null' => true, 'limit' => '255'***REMOVED***);
+            $table->addColumn($columnName, 'string', ['null' => true, 'limit' => '50'***REMOVED***);
         }
 
         foreach ($columns['date'***REMOVED*** as $columnName) {
@@ -103,7 +103,7 @@ class AllColumnsDb extends AbstractMigration
         $table2 = $this->table('all_columns_db_unique_not_null', ['id' => 'id_all_columns_db_unique_not_null'***REMOVED***);
 
         foreach ($columns['string'***REMOVED*** as $columnName) {
-            $table2->addColumn($columnName.'_unique_not_null', 'string', array_merge($nullable, ['limit' => '255'***REMOVED***));
+            $table2->addColumn($columnName.'_unique_not_null', 'string', array_merge($nullable, ['limit' => '50'***REMOVED***));
         }
 
         foreach ($columns['date'***REMOVED*** as $columnName) {
@@ -162,7 +162,7 @@ class AllColumnsDb extends AbstractMigration
         $table2 = $this->table('all_columns_db_unique', ['id' => 'id_all_columns_db_unique'***REMOVED***);
 
         foreach ($columns['string'***REMOVED*** as $columnName) {
-            $table2->addColumn($columnName.'_unique', 'string', ['null' => true, 'limit' => '255'***REMOVED***);
+            $table2->addColumn($columnName.'_unique', 'string', ['null' => true, 'limit' => '50'***REMOVED***);
         }
 
         foreach ($columns['date'***REMOVED*** as $columnName) {
@@ -220,7 +220,7 @@ class AllColumnsDb extends AbstractMigration
         $table2 = $this->table('all_columns_db_not_null', ['id' => 'id_all_columns_db_not_null'***REMOVED***);
 
         foreach ($columns['string'***REMOVED*** as $columnName) {
-            $table2->addColumn($columnName.'_not_null', 'string', ['null' => false, 'limit' => '255'***REMOVED***);
+            $table2->addColumn($columnName.'_not_null', 'string', ['null' => false, 'limit' => '50'***REMOVED***);
         }
 
         foreach ($columns['date'***REMOVED*** as $columnName) {
