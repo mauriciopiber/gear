08619a0d63a2d965e31c4c737f452d8360e654c8
Mauricio Piber Fão <mauriciopiber@gmail.com>
2016-07-22 02:03:23 -0300

    Nomes certos

--- test/unit/GearTest/AllColumnsDbUniqueTableTrait.php
+++ test/unit/GearTest/AllColumnsDbUniqueTableTrait.php
@@ -38,7 +38,7 @@ trait AllColumnsDbUniqueTableTrait
         return $unique->reveal();
     }
 
-    public function getAllPossibleUniqueColumns()
+    public function getAllPossibleColumnsUnique()
     {
         $this->string = new \GearBase\Util\String\StringService();
 
@@ -50,128 +50,128 @@ trait AllColumnsDbUniqueTableTrait
         );
 
         //date
-        $columns[1***REMOVED*** = new \Gear\Column\Date\Date($this->prophesizeColumnUnique('table', 'date_unique_column', 'date'));
-        $columns[1***REMOVED***->setUniqueConstraint($this->prophesizeUnique('table', 'date_unique_column'));
+        $columns[1***REMOVED*** = new \Gear\Column\Date\Date($this->prophesizeColumnUnique('table', 'date_column_unique', 'date'));
+        $columns[1***REMOVED***->setUniqueConstraint($this->prophesizeUnique('table', 'date_column_unique'));
 
-        $columns[2***REMOVED*** = new \Gear\Column\Date\DatePtBr($this->prophesizeColumnUnique('table', 'date_pt_br_unique_column', 'date'));
-        $columns[2***REMOVED***->setUniqueConstraint($this->prophesizeUnique('table', 'date_pt_br_unique_column'));
+        $columns[2***REMOVED*** = new \Gear\Column\Date\DatePtBr($this->prophesizeColumnUnique('table', 'date_pt_br_column_unique', 'date'));
+        $columns[2***REMOVED***->setUniqueConstraint($this->prophesizeUnique('table', 'date_pt_br_column_unique'));
 
         //datetime
         $columns[3***REMOVED*** = new \Gear\Column\Datetime\Datetime(
-            $this->prophesizeColumnUnique('table', 'datetime_unique_column', 'datetime')
+            $this->prophesizeColumnUnique('table', 'datetime_column_unique', 'datetime')
         );
-        $columns[3***REMOVED***->setUniqueConstraint($this->prophesizeUnique('table', 'datetime_unique_column'));
+        $columns[3***REMOVED***->setUniqueConstraint($this->prophesizeUnique('table', 'datetime_column_unique'));
 
         $columns[4***REMOVED*** = new \Gear\Column\Datetime\DatetimePtBr(
-            $this->prophesizeColumnUnique('table', 'datetime_pt_br_unique_column', 'datetime')
+            $this->prophesizeColumnUnique('table', 'datetime_pt_br_column_unique', 'datetime')
         );
-        $columns[4***REMOVED***->setUniqueConstraint($this->prophesizeUnique('table', 'datetime_pt_br_unique_column'));
+        $columns[4***REMOVED***->setUniqueConstraint($this->prophesizeUnique('table', 'datetime_pt_br_column_unique'));
 
         //time
         $columns[5***REMOVED*** = new \Gear\Column\Time\Time(
-            $this->prophesizeColumnUnique('table', 'time_unique_column', 'time')
+            $this->prophesizeColumnUnique('table', 'time_column_unique', 'time')
         );
-        $columns[5***REMOVED***->setUniqueConstraint($this->prophesizeUnique('table', 'time_unique_column'));
+        $columns[5***REMOVED***->setUniqueConstraint($this->prophesizeUnique('table', 'time_column_unique'));
 
         $columns[6***REMOVED*** = new \Gear\Column\Decimal\Decimal(
-            $this->prophesizeColumnUnique('table', 'decimal_unique_column', 'decimal')
+            $this->prophesizeColumnUnique('table', 'decimal_column_unique', 'decimal')
         );
-        $columns[6***REMOVED***->setUniqueConstraint($this->prophesizeUnique('table', 'decimal_unique_column'));
+        $columns[6***REMOVED***->setUniqueConstraint($this->prophesizeUnique('table', 'decimal_column_unique'));
 
         $columns[7***REMOVED*** = new \Gear\Column\Decimal\MoneyPtBr(
-            $this->prophesizeColumnUnique('table', 'money_pt_br_unique_column', 'decimal')
+            $this->prophesizeColumnUnique('table', 'money_pt_br_column_unique', 'decimal')
         );
-        $columns[7***REMOVED***->setUniqueConstraint($this->prophesizeUnique('table', 'money_pt_br_unique_column'));
+        $columns[7***REMOVED***->setUniqueConstraint($this->prophesizeUnique('table', 'money_pt_br_column_unique'));
 
         $columns[8***REMOVED*** = new \Gear\Column\Int\Checkbox(
-            $this->prophesizeColumnUnique('table', 'checkbox_unique_column', 'int')
+            $this->prophesizeColumnUnique('table', 'checkbox_column_unique', 'int')
         );
 
 
         $foreignKey = new \Gear\Column\Int\ForeignKey(
-            $this->prophesizeColumnUnique('table', 'id_foreign_key_unique_column', 'int'),
-            $this->prophesizeForeignKeyUnique('table', 'id_foreign_key_unique_column', 'FOREIGN KEY', 'foreign_key_unique_column')
+            $this->prophesizeColumnUnique('table', 'id_foreign_key_column_unique', 'int'),
+            $this->prophesizeForeignKeyUnique('table', 'id_foreign_key_column_unique', 'FOREIGN KEY', 'foreign_key_column_unique')
         );
 
 
 
         $schema = $this->prophesize('Gear\Table\TableService\TableService');
-        $schema->getReferencedTableValidColumnName('foreign_key_unique_column')
-        ->willReturn('foreign_key_unique_column');
+        $schema->getReferencedTableValidColumnName('foreign_key_column_unique')
+        ->willReturn('foreign_key_column_unique');
 
         $foreignKey->setTableService($schema->reveal());
 
         $columns[9***REMOVED*** = $foreignKey;
-        $columns[9***REMOVED***->setUniqueConstraint($this->prophesizeUnique('table', 'id_foreign_key_unique_column'));
+        $columns[9***REMOVED***->setUniqueConstraint($this->prophesizeUnique('table', 'id_foreign_key_column_unique'));
 
         $columns[10***REMOVED*** = new \Gear\Column\Int\Int(
-            $this->prophesizeColumnUnique('table', 'int_unique_column', 'int')
+            $this->prophesizeColumnUnique('table', 'int_column_unique', 'int')
         );
-        $columns[10***REMOVED***->setUniqueConstraint($this->prophesizeUnique('table', 'int_unique_column'));
+        $columns[10***REMOVED***->setUniqueConstraint($this->prophesizeUnique('table', 'int_column_unique'));
 
         $columns[11***REMOVED*** = new \Gear\Column\Text\Html(
-            $this->prophesizeColumnUnique('table', 'html_unique_column', 'text')
+            $this->prophesizeColumnUnique('table', 'html_column_unique', 'text')
         );
 
 
         $columns[12***REMOVED*** = new \Gear\Column\Text\Text(
-            $this->prophesizeColumnUnique('table', 'text_unique_column', 'text')
+            $this->prophesizeColumnUnique('table', 'text_column_unique', 'text')
         );
 
 
         $columns[13***REMOVED*** = new \Gear\Column\Tinyint\Tinyint(
-            $this->prophesizeColumnUnique('table', 'tinyint_unique_column', 'tinyint')
+            $this->prophesizeColumnUnique('table', 'tinyint_column_unique', 'tinyint')
         );
 
 
         $columns[14***REMOVED*** = new \Gear\Column\Tinyint\Checkbox(
-            $this->prophesizeColumnUnique('table', 'checkbox_unique_column', 'tinyint')
+            $this->prophesizeColumnUnique('table', 'checkbox_column_unique', 'tinyint')
         );
 
 
         $columns[15***REMOVED*** = new \Gear\Column\Varchar\Email(
-            $this->prophesizeColumnUnique('table', 'email_unique_column', 'varchar')
+            $this->prophesizeColumnUnique('table', 'email_column_unique', 'varchar')
         );
-        $columns[15***REMOVED***->setUniqueConstraint($this->prophesizeUnique('table', 'email_unique_column'));
+        $columns[15***REMOVED***->setUniqueConstraint($this->prophesizeUnique('table', 'email_column_unique'));
 
         $columns[16***REMOVED*** = new \Gear\Column\Varchar\PasswordVerify(
-            $this->prophesizeColumnUnique('table', 'password_verify_unique_column', 'varchar')
+            $this->prophesizeColumnUnique('table', 'password_verify_column_unique', 'varchar')
         );
 
 
         $columns[17***REMOVED*** = new \Gear\Column\Varchar\Telephone(
-            $this->prophesizeColumnUnique('table', 'telephone_unique_column', 'varchar')
+            $this->prophesizeColumnUnique('table', 'telephone_column_unique', 'varchar')
         );
-        $columns[17***REMOVED***->setUniqueConstraint($this->prophesizeUnique('table', 'telephone_unique_column'));
+        $columns[17***REMOVED***->setUniqueConstraint($this->prophesizeUnique('table', 'telephone_column_unique'));
 
         $columns[18***REMOVED*** = new \Gear\Column\Varchar\UniqueId(
-            $this->prophesizeColumnUnique('table', 'unique_id_unique_column', 'varchar')
+            $this->prophesizeColumnUnique('table', 'unique_id_column_unique', 'varchar')
         );
 
 
         $columns[19***REMOVED*** = new \Gear\Column\Varchar\UploadImage(
-            $this->prophesizeColumnUnique('table', 'upload_image_unique_column', 'varchar')
+            $this->prophesizeColumnUnique('table', 'upload_image_column_unique', 'varchar')
         );
 
 
         $columns[20***REMOVED*** = new \Gear\Column\Varchar\Url(
-            $this->prophesizeColumnUnique('table', 'url_unique_column', 'varchar')
+            $this->prophesizeColumnUnique('table', 'url_column_unique', 'varchar')
         );
-        $columns[20***REMOVED***->setUniqueConstraint($this->prophesizeUnique('table', 'url_unique_column'));
+        $columns[20***REMOVED***->setUniqueConstraint($this->prophesizeUnique('table', 'url_column_unique'));
 
         $varcharColumn = $this->prophesize('Zend\Db\Metadata\Object\ColumnObject');
         $varcharColumn->getDataType()->willReturn('varchar')->shouldBeCalled();
-        $varcharColumn->getName()->willReturn('varchar_unique_column');
+        $varcharColumn->getName()->willReturn('varchar_column_unique');
         $varcharColumn->getTableName()->willReturn('table');
         $varcharColumn->isNullable()->willReturn(true);
         $varcharColumn->getCharacterMaximumLength()->willReturn(45);
 
         $column = new \Gear\Column\Varchar\Varchar($varcharColumn->reveal());
-        $column->setUniqueConstraint($this->prophesizeUnique('table', 'varchar_unique_column'));
+        $column->setUniqueConstraint($this->prophesizeUnique('table', 'varchar_column_unique'));
 
         $columns[***REMOVED*** = $column;
 
-        //$columns[21***REMOVED***->setUniqueConstraint($this->prophesizeUnique('table', 'varchar_unique_column'));
+        //$columns[21***REMOVED***->setUniqueConstraint($this->prophesizeUnique('table', 'varchar_column_unique'));
 
         //varchar
 
