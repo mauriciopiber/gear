9e77f5ca31523846e6ca26817183f0c078e8f31c
Mauricio Piber Fão <mauriciopiber@gmail.com>
2016-07-30 04:01:17 -0300

    pode ter funcionado

--- test/integration/constructor-db-all/project-create-forward.sh
+++ test/integration/constructor-db-all/project-create-forward.sh
@@ -53,6 +53,6 @@ cd $gearpath && sudo php public/index.php gear module construct MyModule $base -
 #cd $modulepath && ant unit-group -Dgroup=controller.create
 #cd $modulepath && ant unit-group -Dgroup=controller.edit
 #cd $modulepath && ant unit-group -Dgroup=IntForeignKeyController
-cd $modulepath && ant unit-group -Dgroup=AllColumnsDbController
+cd $modulepath && ant unit
 #cd $modulepath && ant protractor-tag -Dtag="@all-columns-db"
 
