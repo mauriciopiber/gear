21c19a4aad3724e8fda14ce6b55e60dd990762ca
Mauricio Piber Fão <mauriciopiber@gmail.com>
2016-08-02 20:30:33 -0300

    Corrigir teste unitário que está com erro

---
+++ src/Gear/Mvc/Repository/ColumnInterface/RepositoryInsertTestInterface.php
@@ -0,0 +1,9 @@
+<?php
+namespace Gear\Mvc\Repository\ColumnInterface;
+
+interface RepositoryInsertTestInterface
+{
+    public function getRepositoryTestInsertData();
+
+    public function getRepositoryTestInsertPersist();
+}
