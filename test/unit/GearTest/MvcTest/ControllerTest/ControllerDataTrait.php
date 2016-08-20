3e527909ae0880758405b4ff55d04896b3b8af78
Mauricio Piber Fão <mauriciopiber@gmail.com>
2016-08-19 21:12:22 -0300

    Funcionando namespace e factory db

--- test/unit/GearTest/MvcTest/ControllerTest/ControllerDataTrait.php
+++ test/unit/GearTest/MvcTest/ControllerTest/ControllerDataTrait.php
@@ -3,9 +3,11 @@ namespace GearTest\MvcTest\ControllerTest;
 
 use GearTest\SingleDbTableTrait;
 use GearTest\AllColumnsDbTableTrait;
+use GearTest\LongNameTableTrait;
 
 trait ControllerDataTrait
 {
+    use LongNameTableTrait;
     use SingleDbTableTrait;
     use AllColumnsDbTableTrait;
 
@@ -18,6 +20,7 @@ trait ControllerDataTrait
             [$this->getSingleColumns(), 'single-db-namespace', true, false, false, 'single_db_table', 'invokables', 'Custom\CustomNamespace'***REMOVED***,
             [$this->getSingleColumns(), 'single-db-factory', true, false, false, 'single_db_table', 'factories', null***REMOVED***,
             [$this->getSingleColumns(), 'single-db-factory-namespace', true, false, false, 'single_db_table', 'factories', 'Custom\CustomNamespace'***REMOVED***,
+            [$this->getLongNameTableColumns(), 'long-name-table', true, true, true, 'my_very_long_table_name_example', 'factories', 'Custom\CustomNamespace'***REMOVED***
             //[$this->getAllPossibleColumnsNotNull(), 'all-columsn-db-not-null', false***REMOVED***,
             //[$this->getAllPossibleColumnsUnique(), 'all-columsn-db-unique', true***REMOVED***,
             //[$this->getAllPossibleColumnsUniqueNotNull(), 'all-columsn-db-unique-not-null', false***REMOVED***,
