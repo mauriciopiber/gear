2a01d4c45ab2435956a7bb243fd5e260f5d41852
Mauricio Piber Fão <mauriciopiber@gmail.com>
2016-09-05 16:51:25 -0300

    Corrigido

--- config/module.config.php
+++ config/module.config.php
@@ -53,7 +53,7 @@ return [
     'gear' => [
         'modules' => [
             'gear' => [
-                'version' => '0.2.83',
+                'version' => '0.2.84',
                 'acl' => true,
                 'name' => __NAMESPACE__,
             ***REMOVED***
