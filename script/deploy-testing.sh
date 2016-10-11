d2d3fc0bb53c4da59f86f3fe13eafb4bf438def3
Mauricio Piber Fão <mauriciopiber@gmail.com>
2016-10-11 08:17:10 -0300

    AAAAAAAAAAA primeiro build2

--- script/deploy-testing.sh
+++ script/deploy-testing.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo composer update -o
+composer update -o
 
 database=$(php -r '$global = require_once("config/autoload/global.php"); echo $global["doctrine"***REMOVED***["connection"***REMOVED***["orm_default"***REMOVED***["params"***REMOVED***["dbname"***REMOVED***;')
 username=$(php -r '$local = require_once("config/autoload/local.php"); echo $local["doctrine"***REMOVED***["connection"***REMOVED***["orm_default"***REMOVED***["params"***REMOVED***["user"***REMOVED***;')
