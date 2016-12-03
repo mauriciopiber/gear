cc53a3e680e4d2ebe112763fd43019f095b40ba4
Mauricio Piber Fão <mauriciopiber@gmail.com>
2016-12-03 06:44:18 -0200

    Alterando

--- config/module.config.php
+++ config/module.config.php
@@ -53,7 +53,7 @@ return [
     'gear' => [
         'modules' => [
             'gear' => [
-                'version' => '0.2.92',
+                'version' => '0.2.93',
                 'acl' => true,
                 'name' => __NAMESPACE__,
                 'git' => 'git@bitbucket.org:mauriciopiber/gear.git'
