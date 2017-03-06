a6fde49e60e9d846a71bced187ef32b044d46ef8
Mauricio Piber Fão <mauriciopiber@gmail.com>
2017-03-06 18:27:23 -0300

    teste unitário em CI para prévia da versão

--- src/Gear/Project/ProjectServiceFactory.php
+++ src/Gear/Project/ProjectServiceFactory.php
@@ -10,11 +10,22 @@ class ProjectServiceFactory implements FactoryInterface
     public function createService(ServiceLocatorInterface $serviceLocator)
     {
         $projectService = new ProjectService(
+            $serviceLocator->get('GearBase\GearConfig'),
+            $serviceLocator->get('GearBase\Util\Dir'),
+            $serviceLocator->get('GearBase\Util\File'),
+            $serviceLocator->get('Gear\FileCreator'),
+            $serviceLocator->get('Gear\Edge\DirEdge'),
+            $serviceLocator->get('Gear\Project\Docs\Docs'),
+            $serviceLocator->get('Gear\Util\Prompt\ConsolePrompt'),
+            $serviceLocator->get('Gear\Upgrade\AntUpgrade'),
+            $serviceLocator->get('Gear\Upgrade\NpmUpgrade'),
+            $serviceLocator->get('config'),
+            $serviceLocator->get('Gear\Project\Composer\ComposerService')
             //$serviceLocator->get('config'),
             //$serviceLocator->get('GearBase\Util\String'),
             //$serviceLocator->get('Gear\FileCreator')
         );
         unset($serviceLocator);
-        return $factory;
+        return $projectService;
     }
 }
