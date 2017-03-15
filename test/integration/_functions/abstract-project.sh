ec019b6304f2c96c8d540f0ff64fed0ee62603be
Mauricio Piber Fão <mauriciopiber@gmail.com>
2017-03-15 06:05:11 -0300

    GP-205 -> Primeira ofensiva para resolver

--- test/integration/_functions/abstract-project.sh
+++ test/integration/_functions/abstract-project.sh
@@ -7,48 +7,48 @@ source "$headersDir/abstract.sh"
 
 function getPath
 {
-	project=${1}
-	basepath=$(basepath)
-	echo "$basepath/$project"
+    project=${1}
+    basepath=$(basepath)
+    echo "$basepath/$project"
 }
 
 
 function createProject
 {
-	project=${1}
-	basepath=$(basepath)
-	projectPath=$(getPath "$project")
+    project=${1}
+    basepath=$(basepath)
+    projectPath=$(getPath "$project")
     sudo php public/index.php gear project create $project --basepath=$basepath --force
     
     cd $projectPath 
-    sudo script/deploy-development.sh	
+    sudo script/deploy-development.sh    
 }
 
 function createModuleWeb
 {
-	projectPath=$(getPath "${1}")
-    module=${2}	
+    projectPath=$(getPath "${1}")
+    module=${2}    
 
     cd $projectPath && sudo php public/index.php gear module create "$module" --type=web
 }
 
 function createModuleCli
 {
-	projectPath=$(getPath "${1}")
-    module=${2}	
-        	
-	cd $projectPath && sudo php public/index.php gear module create "$module" --type=cli
+    projectPath=$(getPath "${1}")
+    module=${2}    
+            
+    cd $projectPath && sudo php public/index.php gear module create "$module" --type=cli
 }
 
 function reload
 {
-	projectPath=$(getPath "${1}")
-	cd $projectPath && sudo script/load.sh
+    projectPath=$(getPath "${1}")
+    cd $projectPath && sudo script/load.sh
 }
 
 function testProject
 {
-	projectPath=$(getPath "${1}")
-	cd $projectPath 
-	ant prepare phpcs phpmd phpcpd unit karma protractor 
+    projectPath=$(getPath "${1}")
+    cd $projectPath 
+    ant prepare phpcs phpmd phpcpd unit karma protractor 
 }
