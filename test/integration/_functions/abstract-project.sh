93cf741a7eb8137c18f6828658d91f23e1ff9b01
Mauricio Piber Fão <mauriciopiber@gmail.com>
2017-03-15 05:35:03 -0300

    GP-204 -> Refatorado código do criar Projeto

--- test/integration/_functions/abstract-project.sh
+++ test/integration/_functions/abstract-project.sh
@@ -5,12 +5,19 @@ headersDir="$( cd "$( dirname "${BASH_SOURCE[0***REMOVED***}" )" && pwd )"
 
 source "$headersDir/abstract.sh"
 
+function getPath
+{
+	project=${1}
+	basepath=$(basepath)
+	echo "$basepath/$project"
+}
+
 
 function createProject
 {
 	project=${1}
 	basepath=$(basepath)
-	projectPath="$basepath/$project"
+	projectPath=$(getPath "$project")
     sudo php public/index.php gear project create $project --basepath=$basepath --force
     
     cd $projectPath 
@@ -19,41 +26,29 @@ function createProject
 
 function createModuleWeb
 {
-    project=${1}
-    module=${2}
-	basepath=$(basepath)
-	projectPath="$basepath/$project"
-	
+	projectPath=$(getPath "${1}")
+    module=${2}	
 
     cd $projectPath && sudo php public/index.php gear module create "$module" --type=web
 }
 
 function createModuleCli
 {
-	project=${1}
+	projectPath=$(getPath "${1}")
     module=${2}	
-	basepath=$(basepath)
-	projectPath="$basepath/$project"
-	
+        	
 	cd $projectPath && sudo php public/index.php gear module create "$module" --type=cli
 }
 
 function reload
 {
-	project=${1}
-	basepath=$(basepath)
-	projectPath="$basepath/$project"	
-	
+	projectPath=$(getPath "${1}")
 	cd $projectPath && sudo script/load.sh
-	
 }
 
 function testProject
 {
-    project=${1}
-	basepath=$(basepath)
-	projectPath="$basepath/$project"
-	cd $projectPath && ant prepare phpcs phpmd phpcpd unit karma 
-    cd $projectPath && ant protractor  
-	
+	projectPath=$(getPath "${1}")
+	cd $projectPath 
+	ant prepare phpcs phpmd phpcpd unit karma protractor 
 }
