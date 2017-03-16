e7be00afb3a91e17b2bf7ba9613e1e43b4e42ac7
Mauricio Piber Fão <mauriciopiber@gmail.com>
2017-03-16 17:50:53 -0300

    GP-204 -> Ultimas alterações, agora rodar ProjectMvc no notebook e ver se aparece o erro

--- test/integration/local/project-mvc/create.sh
+++ test/integration/local/project-mvc/create.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 scriptDir="$( cd "$( dirname "${BASH_SOURCE[0***REMOVED***}" )" && pwd )"
-headersDir="$( cd "$( dirname "${BASH_SOURCE[0***REMOVED***}" )" && cd ./../../../_functions && pwd )"
+headersDir="$( cd "$( dirname "${BASH_SOURCE[0***REMOVED***}" )" && cd ./../../_functions && pwd )"
 
 source "$headersDir/abstract-project.sh"
 
 project="ProjectMvc"
 
+deleteProject "$project"
+
 createProject "$project"
+
 createModuleCli "$project" "MyProjectModuleCli"
 createModuleWeb "$project" "MyProjectModuleWeb"
 createModuleWeb "$project" "MyProjectModuleMvc"
---
+++ test/integration/local/project-mvc/reload.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+scriptDir="$( cd "$( dirname "${BASH_SOURCE[0***REMOVED***}" )" && pwd )"
+headersDir="$( cd "$( dirname "${BASH_SOURCE[0***REMOVED***}" )" && cd ./../../_functions && pwd )"
+
+source "$headersDir/abstract-project.sh"
+
+project="ProjectMvc"
+
+removeModuleFromProject "$project" "MyProjectModuleCli"
+removeModuleFromProject "$project" "MyProjectModuleWeb"
+removeModuleFromProject "$project" "MyProjectModuleMvc"
+
+createModuleCli "$project" "MyProjectModuleCli"
+createModuleWeb "$project" "MyProjectModuleWeb"
+createModuleWeb "$project" "MyProjectModuleMvc"
+
+constructModuleProject "$project" "MyProjectModuleCli" "$scriptDir" "module-cli.yml"
+constructModuleProject "$project" "MyProjectModuleWeb" "$scriptDir" "module-web.yml"
+
+prepareConstruct "$project" "$scriptDir/20160123222068_all_columns.php"
+
+constructModuleProject "$project" "MyProjectModuleMvc" "$scriptDir" "module-mvc.yml"
+
+reload "$project"
+
+testProject "$project"
+
+exit 0
