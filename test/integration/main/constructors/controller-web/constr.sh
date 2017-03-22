fdc4f4a022f3adf8cb954b3ba43e493048f0316d
Mauricio Piber Fão <mauriciopiber@gmail.com>
2017-03-22 04:23:39 -0300

    Saving code for module Gear by GearDeploy on 22/03/2017 04:23:37

--- test/integration/main/constructors/controller-web/constr.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-scriptDir="$( cd "$( dirname "${BASH_SOURCE[0***REMOVED***}" )" && pwd )"
-headersDir="$( cd "$( dirname "${BASH_SOURCE[0***REMOVED***}" )" && cd ./../../../_functions && pwd )"
-
-source "$headersDir/abstract-web.sh"
-
-runConstructWeb "ControllerWeb" "$scriptDir/simpliest-controller.yml"
-
