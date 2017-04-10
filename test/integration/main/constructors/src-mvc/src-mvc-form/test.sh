c723f4f047e6836ae5cbf0cf012ad5dce74d183c
Mauricio Piber Fão <mauriciopiber@gmail.com>
2017-04-10 18:20:01 -0300

    Saving code for module Gear by GearDeploy on 10/04/2017 18:19:58

--- test/integration/main/constructors/src-mvc/src-mvc-form/test.sh
+++ test/integration/main/constructors/src-mvc/src-mvc-form/test.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
+set -e
+
 bin="$( cd "$( dirname "${BASH_SOURCE[0***REMOVED***}" )" && cd ./../../../../../../bin && pwd )"
 scriptDir="$( cd "$( dirname "${BASH_SOURCE[0***REMOVED***}" )" && pwd )"
 
-module="PbrSrcMvcService"
+module="PbrSrcMvcForm"
 type="web"
 build=${2}
 
 construct=()
 construct[0***REMOVED***="../src-mvc-entity/gear-entity.yml;../src-mvc-entity/20160123222001_entity_database.php"
-construct[1***REMOVED***="../src-mvc-fixture/gear-fixture.yml;"
-construct[2***REMOVED***="../src-mvc-repository/gear-repository.yml;"
-construct[3***REMOVED***="gear-service.yml;"
+#construct[1***REMOVED***="../src-mvc-fixture/gear-fixture.yml;"
+#construct[2***REMOVED***="gear-repository.yml;"
+construct[1***REMOVED***="../src-mvc-filter/gear-filter.yml;"
+construct[2***REMOVED***="gear-form.yml;"
 
 source "$bin/gear/module/test-module.sh"
-
