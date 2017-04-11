46099da9830fb92a68cc7d882da23353b7e153d6
Mauricio Piber Fão <mauriciopiber@gmail.com>
2017-04-11 10:02:33 -0300

    aquela build demanhã só pra ver como tá

--- test/integration/main/constructors/src/src-interface/test.sh
+++ test/integration/main/constructors/src/src-interface/test.sh
@@ -2,12 +2,13 @@
 
 set -e
 
-bin="$( cd "$( dirname "${BASH_SOURCE[0***REMOVED***}" )" && cd ./../../../../../bin && pwd )"
+bin="$( cd "$( dirname "${BASH_SOURCE[0***REMOVED***}" )" && cd ./../../../../../../bin && pwd )"
 scriptDir="$( cd "$( dirname "${BASH_SOURCE[0***REMOVED***}" )" && pwd )"
 
 module="SrcInterface"
 type="cli"
 migration=""
 gearfile="gear-interface.yml"
+build=${2}
 
 source "$bin/gear/module/test-module.sh"
