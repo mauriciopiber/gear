68636a6de02d47568117bd7be28f5b2c24b26f38
Mauricio Piber Fão <mauriciopiber@gmail.com>
2017-04-13 14:27:25 -0300

    Saving code for module Gear by GearDeploy on 13/04/2017 14:27:25

--- bin/gear/abstract/abstract-module.sh
+++ bin/gear/abstract/abstract-module.sh
@@ -136,8 +136,8 @@ function Gear_Module_Integrate
     Gear_Module_Construct "$module" "$type" "$scriptDir" "$construct" "0" "0"
 
     if [ "$type" == "web" ***REMOVED***; then
-    	cd $modulePath && php public/index.php gear project fixture
-        cd $modulePath && php public/index.php gear project setUpAcl --memcached
+    	cd $modulePath && sudo php public/index.php gear project fixture
+        cd $modulePath && sudo php public/index.php gear project setUpAcl --memcached
         cd $modulePath && sudo php public/index.php gear module load BjyAuthorize --after=ZfcUserDoctrineORM
         sudo vendor/bin/gulp
     fi
