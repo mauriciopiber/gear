e5d232ad20c8f9620e2f9fe431736528eb1a858b
Mauricio Piber Fão <mauriciopiber@gmail.com>
2017-04-19 21:04:30 -0300

    Saving code for module Gear by GearDeploy on 19/04/2017 21:04:29

--- test/integration/main/constructors/src/src-controller-plugin/test.sh
+++ test/integration/main/constructors/src/src-controller-plugin/test.sh
@@ -5,9 +5,9 @@ set -e
 bin="$( cd "$( dirname "${BASH_SOURCE[0***REMOVED***}" )" && cd ./../../../../../../bin && pwd )"
 scriptDir="$( cd "$( dirname "${BASH_SOURCE[0***REMOVED***}" )" && pwd )"
 
-module="PbrSrcValueObject"
+module="PbrSrcControllerPlugin"
 type="cli"
 construct=()
-construct[1***REMOVED***="valueobject.yml;"
+construct[1***REMOVED***="src-controller-plugin.yml;"
 
 source "$bin/gear/module/test-module.sh"
--- test/integration/main/constructors/src/src-value-object/test.sh
+++ test/integration/main/constructors/src/src-value-object/test.sh
@@ -5,9 +5,9 @@ set -e
 bin="$( cd "$( dirname "${BASH_SOURCE[0***REMOVED***}" )" && cd ./../../../../../../bin && pwd )"
 scriptDir="$( cd "$( dirname "${BASH_SOURCE[0***REMOVED***}" )" && pwd )"
 
-module="PbrSrcControllerPlugin"
+module="PbrSrcValueObject"
 type="cli"
 construct=()
-construct[1***REMOVED***="controllerplugin.yml;"
+construct[1***REMOVED***="src-value-object.yml;"
 
 source "$bin/gear/module/test-module.sh"
