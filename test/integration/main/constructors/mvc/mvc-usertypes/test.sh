022226d5a7fc31d6f666b80ccefbeee9b0416a4a
Mauricio Piber Fão <mauriciopiber@gmail.com>
2017-04-25 15:48:43 -0300

    Saving code for module Gear by GearDeploy on 25/04/2017 15:48:42

--- test/integration/main/constructors/mvc/mvc-usertypes/test.sh
+++ test/integration/main/constructors/mvc/mvc-usertypes/test.sh
@@ -6,7 +6,7 @@ scriptDir="$( cd "$( dirname "${BASH_SOURCE[0***REMOVED***}" )" && pwd )"
 module="PbrMvcUsertypes"
 type="web"
 construct=()
-construct+=("/:/")
+construct+=("mvc-basic-strict/mvc-basic-strict.yml:mvc-basic-strict/mvc_basic_strict.php")
 
 
 source "$bin/gear/module/test-module.sh"
