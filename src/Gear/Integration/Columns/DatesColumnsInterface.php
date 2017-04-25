42a8c2e3c057a071457511ca50b1ff63c1670d64
Mauricio Piber Fão <mauriciopiber@gmail.com>
2017-04-25 18:24:48 -0300

    Saving code for module Gear by GearDeploy on 25/04/2017 18:24:48

---
+++ src/Gear/Integration/Columns/DatesColumnsInterface.php
@@ -0,0 +1,34 @@
+<?php
+namespace Gear\Integration\Columns;
+
+/**
+ * PHP Version 5
+ *
+ * @category Interface
+ * @package Gear/Integration/Columns
+ * @author Mauricio Piber <mauriciopiber@gmail.com>
+ * @license GPL3-0 http://www.gnu.org/licenses/gpl-3.0.en.html
+ * @link http://pibernetwork.com
+ */
+interface DatesColumnsInterface
+{
+    // TIME
+    const default_time           = 'clm_tme';
+
+    //DATETIME
+     const default_datetime      = 'clm_dtt';
+     const default_datetime_ptbr = 'clm_dtt_pt';
+
+     // DATE
+     const default_date          = 'clm_dte';
+     const default_date_ptbr     = self::default_date.'_pt';
+
+    const COLUMNS = [
+
+        self::default_time              => ['type' => 'time'***REMOVED***,
+        self::default_datetime          => ['type' => 'datetime'***REMOVED***,
+        self::default_datetime_ptbr     => ['type' => 'datetime', 'speciality' => 'datetime-pt-br'***REMOVED***,
+        self::default_date              => ['type' => 'date'***REMOVED***,
+        self::default_date_ptbr         => ['type' => 'date', 'speciality' => 'date-pt-br'***REMOVED***
+    ***REMOVED***
+}
