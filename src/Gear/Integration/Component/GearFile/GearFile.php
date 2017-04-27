0429efd39f2d8d1041021cad132d0c9ba2c5e660
Mauricio Piber Fão <mauriciopiber@gmail.com>
2017-04-27 04:10:35 -0300

    Saving code for module Gear by GearDeploy on 27/04/2017 04:10:35

--- src/Gear/Integration/Component/GearFile/GearFile.php
+++ src/Gear/Integration/Component/GearFile/GearFile.php
@@ -7,6 +7,8 @@ use Gear\Integration\Util\Persist\Persist;
 use GearBase\Util\String\StringService;
 use Symfony\Component\Yaml\Yaml;
 use Gear\Integration\Suite\Mvc\MvcMinorSuite;
+use Gear\Integration\Suite\Src\SrcMinorSuite;
+use Gear\Integration\Util\Numbers\NumberToStringInterface;
 
 /**
  * PHP Version 5
@@ -40,23 +42,6 @@ class GearFile
 
         return $this;
     }
-
-    private function createGearfileComponent($suite, $data)
-    {
-        $suiteName = $suite->getSuiteName();
-
-        $name = $this->stringService->str('url', $suiteName);
-
-        $gearfile = sprintf('%s.yml', $name);
-
-        $yaml = Yaml::dump($data);
-
-        $this->persist->save($suite, $gearfile, $yaml);
-
-        echo "Criado com sucesso"."\n";
-        return $gearfile;
-    }
-
 
     public function createMvcGearfile(MvcMinorSuite $mvcMinorSuite)
     {
@@ -116,11 +101,146 @@ class GearFile
         ***REMOVED***;
     }
 
-
-    public function createSrcGearfile(SrcMinorSuite $suite)
+    private function createGearfileComponent($suite, $data)
     {
+        $suiteName = $suite->getSuiteName();
+
+        $name = $this->stringService->str('url', $suiteName);
+
+        $gearfile = sprintf('%s.yml', $name);
+
+        $yaml = Yaml::dump($data);
+
+        $this->persist->save($suite, $gearfile, $yaml);
+
+        echo "Criado com sucesso"."\n";
+        return $gearfile;
+    }
+
+
+    public function createSrcGearfile(SrcMinorSuite $suite, $srcOptions)
+    {
+        $data = [***REMOVED***;
+
+        foreach ($srcOptions as $options) {
+            $data = array_merge($data, $this->generateGearfiles($options[0***REMOVED***, $options[1***REMOVED***, $options[2***REMOVED***, $options[3***REMOVED***));
+        }
+
+        return $this->createGearfileComponent($suite, ['src' => $data***REMOVED***);
+    }
+
+
+    private function generateGearfiles($invokables, $config, $type, $repeat)
+    {
+        $invokableFile = [***REMOVED***;
+
+        foreach ($invokables as $invokable) {
+
+            foreach ($config as $configName) {
+
+                for ($i = 1; $i <= $repeat; $i++) {
+
+                    $invokableFile[***REMOVED*** = $this->generateSource($invokable, $configName, $type, $i);
+                }
+            }
+        }
+
+        return $invokableFile;
+
 
     }
+
+    private function generateSource($invokable, $configName, $type, $repeat)
+    {
+
+        $numberString = NumberToStringInterface::NUMBER_MAP[$repeat***REMOVED***;
+
+        $name = sprintf($invokable['name'***REMOVED***, $type, $this->stringService->str('class', substr($configName, 0, 5)), $numberString);
+
+        $entry = ['name' => $name, 'type' => $invokable['type'***REMOVED******REMOVED***;
+
+        if (isset($invokable['extends'***REMOVED***)) {
+            $entry['extends'***REMOVED*** = sprintf($invokable['extends'***REMOVED***, $type, $type, $this->stringService->str('class', substr($configName, 0, 5)), $numberString);
+        }
+
+        if (isset($invokable['namespace'***REMOVED***)) {
+            $entry['namespace'***REMOVED*** = $this->createNamespace($type, $repeat);
+
+        }
+
+        if (isset($invokable['implements'***REMOVED***)) {
+
+            if (is_array($invokable['implements'***REMOVED***)) {
+
+               $entry['implements'***REMOVED*** = [***REMOVED***;
+
+               foreach ($invokable['implements'***REMOVED*** as $invokDep) {
+                   $entry['implements'***REMOVED***[***REMOVED*** = sprintf($invokDep, $type, $numberString);
+               }
+
+            } else {
+                $entry['implements'***REMOVED*** = sprintf($invokable['implements'***REMOVED***, $type, $numberString);
+            }
+        }
+
+        if ($configName !== 'abstract' && $invokable['type'***REMOVED*** !== 'Interface') {
+            $entry['service'***REMOVED*** = $configName;
+        } elseif ($invokable['type'***REMOVED*** !== 'Interface') {
+            $entry['abstract'***REMOVED*** = true;
+        }
+
+
+        if (isset($invokable['dependency'***REMOVED***)) {
+            if (is_array($invokable['dependency'***REMOVED***)) {
+
+               $entry['dependency'***REMOVED*** = [***REMOVED***;
+
+               foreach ($invokable['dependency'***REMOVED*** as $invokDep) {
+                   $entry['dependency'***REMOVED***[***REMOVED*** = sprintf($invokDep, $type, $type, $numberString);
+               }
+
+            } else {
+                $entry['dependency'***REMOVED*** = sprintf($invokable['dependency'***REMOVED***, $type, $type, $numberString);
+            }
+        }
+
+        return $entry;
+    }
+
+    public function createNamespace($type, $number)
+    {
+        $textName = '';
+
+        for ($x = 1; $x <= $number; $x++) {
+
+            if (!empty($textName)) {
+                $textName .= '\\';
+            }
+            $textName .= sprintf('%s%s', substr($type, 0, 5), NumberToStringInterface::NUMBER_MAP[$x***REMOVED***);
+
+        }
+
+
+        return $textName;
+    }
+
+    public function createMultiplesInterfaces($type, $repeat)
+    {
+        $interfaceString = 'Interfaces\%sImpl%s';
+
+        if ($repeat == 1) {
+            return sprintf($interfaceString, $type, NumberToStringInterface::NUMBER_MAP[1***REMOVED***);
+        }
+
+        $interfaces = [***REMOVED***;
+
+        for ($z = 1; $z <= $repeat; $z++) {
+            $interfaces[***REMOVED*** = sprintf($interfaceString, $type, NumberToStringInterface::NUMBER_MAP[$z***REMOVED***);
+        }
+
+        return $interfaces;
+    }
+
 
     /**
     public function createSrcMvcGearfile(SrcMvcMinorSuite $suite)
