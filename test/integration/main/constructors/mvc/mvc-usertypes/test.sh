13867ec211e6396ca28eeec117688589574f58cd
Mauricio Piber Fão <mauriciopiber@gmail.com>
2017-05-05 10:28:15 -0300

    Saving code for module Gear by GearDeploy on 05/05/2017 10:28:15

--- test/integration/main/constructors/mvc/mvc-usertypes/test.sh
+++ test/integration/main/constructors/mvc/mvc-usertypes/test.sh
@@ -6,6 +6,7 @@ scriptDir="$( cd "$( dirname "${BASH_SOURCE[0***REMOVED***}" )" && pwd )"
 module="PbrMvcUsertypes"
 type="web"
 construct=()
+construct+=("mvc-basic-low-strict/mvc-basic-low-strict.yml:mvc-basic-low-strict/mvc_basic_low_strict.php")
 construct+=("mvc-basic-strict/mvc-basic-strict.yml:mvc-basic-strict/mvc_basic_strict.php")
 
 
--- test/integration/main/constructors/src-mvc/test.sh
+++ test/integration/main/constructors/src-mvc/test.sh
@@ -6,6 +6,12 @@ scriptDir="$( cd "$( dirname "${BASH_SOURCE[0***REMOVED***}" )" && pwd )"
 module="Pbr"
 type="web"
 construct=()
+construct+=("src-mvc-entity/src-mvc-entity.yml:src-mvc-entity/src_mvc_entity.php")
+construct+=("src-mvc-fixture/src-mvc-fixture.yml")
+construct+=("src-mvc-repository/src-mvc-repository.yml")
+construct+=("src-mvc-service/src-mvc-service.yml")
+construct+=("src-mvc-filter/src-mvc-filter.yml")
+construct+=("src-mvc-form/src-mvc-form.yml")
 construct+=("src-mvc-search-form/src-mvc-search-form.yml")
 
 
