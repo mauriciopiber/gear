52ed1284d4c19ca7cab76602daf60d11e70cfb53
Mauricio Piber Fão <mauriciopiber@gmail.com>
2017-05-23 02:51:07 -0300

    Saving code for module Gear by GearDeploy on 23/05/2017 02:51:07

---
+++ test/integration/main/constructors/mvc/mvc-upload-image/mvc-basic-upload-image/test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+bin="$( cd "$( dirname "${BASH_SOURCE[0***REMOVED***}" )" && cd ./../../../../../../../bin && pwd )"
+scriptDir="$( cd "$( dirname "${BASH_SOURCE[0***REMOVED***}" )" && pwd )"
+
+module="PbrMvcUploadImageBasicUploadImage"
+type="web"
+construct=()
+construct+=("mvc-upload-image-basic-upload-image.yml;mvc_upload_image_basic_upload_image.php")
+
+source "$bin/gear/module/test-module.sh"
