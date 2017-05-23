fae52341326b4fe057c9bfaca12c0df2e8d5569d
Mauricio Piber Fão <mauriciopiber@gmail.com>
2017-05-23 19:31:44 -0300

    Saving code for module Gear by GearDeploy on 23/05/2017 19:31:44

--- test/unit/GearTest/IntegrationTest/SuiteTest/ControllerMvcTest/ControllerMvcMinorSuiteTest.php
+++ test/unit/GearTest/IntegrationTest/SuiteTest/ControllerMvcTest/ControllerMvcMinorSuiteTest.php
@@ -27,4 +27,89 @@ class ControllerMvcMinorSuiteTest extends TestCase
         $this->assertEquals('controller-mvc/controller-mvc', $this->controllerMvcMinorSuite->getLocationKey());
         $this->assertTrue($this->controllerMvcMinorSuite->isUsingLongName());
     }
+
+
+    public function getSuiteData()
+    {
+        $data = [***REMOVED***;
+        $data[***REMOVED*** = new ControllerMvcMinorSuite(new ControllerMvcMajorSuite(), 'basic', 'all', null, null, true);
+        $data[***REMOVED*** = new ControllerMvcMinorSuite(new ControllerMvcMajorSuite(), 'complete', 'strict', 'unique', 'upload_image', true);
+        $data[***REMOVED*** = new ControllerMvcMinorSuite(new ControllerMvcMajorSuite(), 'basic', 'all', null, null, false);
+        $data[***REMOVED*** = new ControllerMvcMinorSuite(new ControllerMvcMajorSuite(), 'complete', 'strict', 'unique', 'upload_image', false);
+        return $data;
+    }
+
+    public function createDataSet($expected)
+    {
+        $data = $this->getSuiteData();
+
+        $dataSet = [***REMOVED***;
+
+        foreach ($data as $key => $item) {
+            $dataSet[***REMOVED*** = [$item, $expected[$key***REMOVED******REMOVED***;
+        }
+
+        return $dataSet;
+    }
+
+    public function getTableNameSuite()
+    {
+        $expected = [
+            'SrcMvcBasic',
+            'SrcMvcCompleteStrictUniqueUploadImage',
+            'SrcMvcBasic',
+            'SrcMvcCompleteStrictUniqueUploadImage',
+        ***REMOVED***;
+
+        return $this->createDataSet($expected);
+    }
+
+    public function getTableAliasSuite()
+    {
+        $expected = [
+            'SrcMvcBasic',
+            'SrcMvcCompleteStrictUniqueUploadImage',
+            'SrcMvcBsc',
+            'SrcMvcCmpStrUniUpl',
+        ***REMOVED***;
+
+        return $this->createDataSet($expected);
+    }
+
+    public function getTableLocationKeySuite()
+    {
+        $expected = [
+            'controller-mvc/controller-mvc',
+            'controller-mvc/controller-mvc',
+            'controller-mvc/controller-mvc',
+            'controller-mvc/controller-mvc',
+        ***REMOVED***;
+
+        return $this->createDataSet($expected);
+    }
+
+    /**
+     * @dataProvider getTableLocationKeySuite
+     *
+     */
+    public function testTableLocationKey($suite, $expected)
+    {
+        $this->assertEquals($expected, $suite->getLocationKey());
+    }
+
+    /**
+     * @dataProvider getTableAliasSuite
+     */
+    public function testTableAlias($suite, $expected)
+    {
+        $this->assertEquals($expected, $suite->getTableAlias());
+    }
+
+    /**
+     * @dataProvider getTableNameSuite
+     */
+    public function testTableName($suite, $expected)
+    {
+        $this->assertEquals($expected, $suite->getTableName());
+    }
 }
