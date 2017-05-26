cef50d5238b244d2f7ba05fa9a0a4caeba11e9c8
Mauricio Piber Fão <mauriciopiber@gmail.com>
2017-05-26 05:49:23 -0300

    Saving code for module Gear by GearDeploy on 26/05/2017 05:49:23

--- test/integration/fast-test.sh
+++ test/integration/fast-test.sh
@@ -1,9 +1,19 @@
 
-test/integration/main/constructors/mvc/mvc-usertypes/mvc-basic-strict/test.sh reset
-test/integration/main/constructors/mvc/mvc-usertypes/mvc-basic-strict/test.sh reconstruct "parallel-lint"
-test/integration/main/constructors/mvc/mvc-usertypes/mvc-basic-strict/test.sh test "${1}"
+if [ "${1}" == "" ***REMOVED***; then
+	
+	testName="protractor"
+else 
+    testName="${1}"	
+fi
 
-test/integration/main/constructors/mvc/mvc-usertypes/mvc-basic-low-strict/test.sh reset
+	
+	
+
+#test/integration/main/constructors/mvc/mvc-usertypes/mvc-basic-strict/test.sh reset
+test/integration/main/constructors/mvc/mvc-usertypes/mvc-basic-strict/test.sh reconstruct "parallel-lint"
+test/integration/main/constructors/mvc/mvc-usertypes/mvc-basic-strict/test.sh test "$testName"
+
+#test/integration/main/constructors/mvc/mvc-usertypes/mvc-basic-low-strict/test.sh reset
 test/integration/main/constructors/mvc/mvc-usertypes/mvc-basic-low-strict/test.sh reconstruct "parallel-lint"
-test/integration/main/constructors/mvc/mvc-usertypes/mvc-basic-low-strict/test.sh test "${1}"
+test/integration/main/constructors/mvc/mvc-usertypes/mvc-basic-low-strict/test.sh test "$testName"
 
