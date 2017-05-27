db1c2564b5a91e0994926d6cd51bc71f413a4b8c
Mauricio Piber Fão <mauriciopiber@gmail.com>
2017-05-27 05:58:23 -0300

    Saving code for module Gear by GearDeploy on 27/05/2017 05:58:20

--- test/integration/fast-test.sh
+++ test/integration/fast-test.sh
@@ -7,9 +7,9 @@ fi
 
 
 modules=(
-    "mvc/mvc-columns/mvc-varchar"
-    "mvc/mvc-columns/mvc-numeric"
-    "mvc/mvc-columns/mvc-text"
+#    "mvc/mvc-columns/mvc-varchar"
+#    "mvc/mvc-columns/mvc-numeric"
+#    "mvc/mvc-columns/mvc-text"
     "mvc/mvc-columns/mvc-dates"
 )
 
@@ -17,7 +17,7 @@ modules=(
 for item in ${modules[****REMOVED***}
 do
     test/integration/main/constructors/$item/test.sh reset
-    test/integration/main/constructors/$item/test.sh custom 'sudo composer update'
+#    test/integration/main/constructors/$item/test.sh custom 'sudo composer update'
     test/integration/main/constructors/$item/test.sh reconstruct "parallel-lint"
     test/integration/main/constructors/$item/test.sh test "$testName"
 
