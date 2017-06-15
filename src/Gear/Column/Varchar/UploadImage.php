97cd34cba79d484c043a57b4a508319f863ea86f
Mauricio Piber Fão <mauriciopiber@gmail.com>
2017-06-15 10:54:19 -0300

    Saving code for module Gear by GearDeploy on 15/06/2017 10:54:19

--- src/Gear/Column/Varchar/UploadImage.php
+++ src/Gear/Column/Varchar/UploadImage.php
@@ -189,7 +189,7 @@ EOS;
     {
         $table = $this->str('url', $this->column->getTableName());
         $column = $this->str('var', $this->column->getName());
-        return sprintf('upload/%s-%s/%02d-%s-%s.gif', $table, $column, $iterator, '%s', $column);
+        return sprintf('upload/%s-%s/%02d-%s-%s-%s.gif', $table, $column, $iterator, '%s', $table, $column);
     }
 
     /**
@@ -204,7 +204,7 @@ EOS;
     {
         $table = $this->str('url', $this->column->getTableName());
         $column = $this->str('var', $this->column->getName());
-        return sprintf('/upload/%s-%s/%02d-pre-%s.gif', $table, $column, $iterator, $column);
+        return sprintf('/upload/%s-%s/%02d-pre-%s-%s.gif', $table, $column, $iterator, $table, $column);
     }
 
     /**
