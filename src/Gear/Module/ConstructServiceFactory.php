ed9bde86bd39434e04d1c822ff45a5296d54e239
Mauricio Piber Fão <mauriciopiber@gmail.com>
2017-06-19 17:54:24 -0300

    Saving code for module Gear by GearDeploy on 19/06/2017 17:54:23

--- src/Gear/Module/ConstructServiceFactory.php
+++ src/Gear/Module/ConstructServiceFactory.php
@@ -4,23 +4,23 @@ namespace Gear\Module;
 use Zend\ServiceManager\FactoryInterface;
 use Zend\ServiceManager\ServiceLocatorInterface;
 #use Gear\Module\ConstructorServiceFactory;
-use Gear\Module\ConstructorService;
+use Gear\Module\ConstructService;
 
 class ConstructServiceFactory implements FactoryInterface
 {
     public function createService(ServiceLocatorInterface $serviceLocator)
     {
-        return new ConstructorService(
+        return new ConstructService(
             $serviceLocator->get('GearJson\Db'),
             $serviceLocator->get('GearJson\Src'),
+            $serviceLocator->get('GearJson\App'),
             $serviceLocator->get('GearJson\Controller'),
             $serviceLocator->get('GearJson\Action'),
-            $serviceLocator->get('GearJson\App'),
             $serviceLocator->get('Gear\Module\Constructor\Db'),
             $serviceLocator->get('Gear\Module\Constructor\Src'),
+            $serviceLocator->get('Gear\Module\Constructor\App'),
             $serviceLocator->get('Gear\Module\Constructor\Controller'),
-            $serviceLocator->get('Gear\Module\Constructor\Action'),
-            $serviceLocator->get('Gear\Module\Constructor\App')
+            $serviceLocator->get('Gear\Module\Constructor\Action')
         );
     }
 }
