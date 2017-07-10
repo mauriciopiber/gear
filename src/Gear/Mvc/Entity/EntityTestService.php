2ebb71ef59c6e8b5e75f5fa60b8cd6b60c7a6f04
Mauricio Piber Fão <mauriciopiber@gmail.com>
2017-07-10 02:46:39 -0300

    Saving code for module Gear by GearDeploy on 10/07/2017 02:46:37

--- src/Gear/Mvc/Entity/EntityTestService.php
+++ src/Gear/Mvc/Entity/EntityTestService.php
@@ -19,9 +19,6 @@ use Gear\Column\Integer\PrimaryKey;
 
 class EntityTestService extends AbstractMvc
 {
-    protected $mockColumns;
-
-
     public function introspectFromTable(Db $db)
     {
         $this->db = $db;
@@ -40,233 +37,36 @@ class EntityTestService extends AbstractMvc
         $this->tableName = $this->str('uline', $this->db->getTable());
         $this->columnManager = $this->db->getColumnManager();
 
-        $entityTestConfig = new EntityTestConfig(
-            $this->getModule()->getModuleName(),
-            $this->str('class', $this->tableName)
-        );
 
-        $this->createTestFieldsNullMethod($entityTestConfig);
+        $options = [
+            'module' => $this->getModule()->getModuleName()
+        ***REMOVED***;
 
-        $this->createTestFieldsMethod($entityTestConfig);
+        $options['params'***REMOVED*** = $this->columnManager->extractCode('getEntityParam', [***REMOVED***, [
+            \Gear\Column\Integer\PrimaryKey::class
+        ***REMOVED***);
 
-        $this->createTestFieldsProviderMethod($entityTestConfig);
+        $options['assertNull'***REMOVED*** = $this->columnManager->extractCode('getEntityAssertNull', [***REMOVED***);
+
+        $options['asserts'***REMOVED*** =  $this->columnManager->extractCode('getEntitySetter', [***REMOVED***, [
+            \Gear\Column\Integer\PrimaryKey::class
+        ***REMOVED***);
+
+        $options['mocks'***REMOVED*** = $this->columnManager->extractCode('getEntityMock', [***REMOVED***, [
+            \Gear\Column\Integer\PrimaryKey::class
+        ***REMOVED***);
+
+        $options['exports'***REMOVED*** = $this->columnManager->extractCode('getEntityDataProvider', [***REMOVED***, [
+            \Gear\Column\Integer\PrimaryKey::class
+        ***REMOVED***);
+
+        $options['className'***REMOVED*** = $this->str('class', $this->tableName);
 
         return $this->getFileCreator()->createFile(
             'template/module/mvc/entity-test/src.entity.phtml',
-            $entityTestConfig->export(),
+            $options,
             $this->str('class', $this->tableName).'Test.php',
             $this->getModule()->getTestEntityFolder()
         );
     }
-
-    public function createTestFieldsProviderMethod(EntityTestConfig &$entityTestConfig)
-    {
-        $provider = $this->getProvider();
-        $mocks = $this->getMocks();
-        $entityTestConfig->setFieldsProviderMethod(
-            $mocks,
-            $provider
-        );
-    }
-
-    public function createTestFieldsMethod(EntityTestConfig &$entityTestConfig)
-    {
-        $entityTestConfig->setFieldsMethod(
-            $this->getParams(),
-            $this->getTestSetters()
-        );
-    }
-
-    public function createTestFieldsNullMethod(EntityTestConfig &$entityTestConfig)
-    {
-        //$assertNull = [***REMOVED***;
-
-        //$useMethods = [***REMOVED***;
-
-        $assertNull = $this->columnManager->extractCode('getEntityAssertNull', [***REMOVED***);
-
-        /*
-        //lista todas colunas
-        foreach ($this->tableColumns as $columnData) {
-
-        }
-        */
-
-        /*
-        $moreMethodsUse = $this->getExtraGetter($useMethods);
-
-        if (count($moreMethodsUse)>0) {
-            foreach ($moreMethodsUse as $method) {
-                $assertNull[***REMOVED*** = sprintf(
-                    '$this->assertInstanceOf(\'Doctrine\Common\Collections\ArrayCollection\',$this->entity->%s());',
-                    $method
-                );
-            }
-        }
-        */
-
-        $entityTestConfig->setFieldsNullMethod($assertNull);
-    }
-
-    /**
-     * Used by $entity->testGetterInitiateByNull()
-     *
-     * @return string[***REMOVED***
-     */
-    public function getTestSetters()
-    {
-        //$testSetters = [***REMOVED***;
-        return $this->columnManager->extractCode('getEntitySetter', [***REMOVED***, [
-            \Gear\Column\Integer\PrimaryKey::class
-        ***REMOVED***);
-
-        /*
-        $moreMethodsUse = $this->getExtraSetter();
-
-        if (count($moreMethodsUse)>0) {
-            foreach ($moreMethodsUse as $newMock) {
-                $classId    = str_replace('add', '', $newMock);
-
-                $assertNull[***REMOVED*** = sprintf(
-                    '$this->entity->add%s($%s);',
-                    $this->str('class', $classId),
-                    $this->str('var-length', $classId)
-                );
-                $assertNull[***REMOVED*** = sprintf(
-                    '$this->entity->remove%s($%s);',
-                    $this->str('class', $classId),
-                    $this->str('var-length', $classId)
-                );
-            }
-        }
-        //var_dump($moreMethodsUse);
-
-
-        return $assertNull;
-        */
-    }
-
-    public function getExtraSetter()
-    {
-        $classMethods = $this->getClassMethods();
-
-
-        $filter = function ($value) {
-            if (substr($value, 0, 3) === 'add') {
-                return true;
-            } else {
-                return false;
-            }
-        };
-
-        return array_filter($classMethods, $filter);
-    }
-
-
-    public function getProvider()
-    {
-        //$this->columnManager->extractCode('getEntityDataProvider', [***REMOVED***);
-        return $this->columnManager->extractCode('getEntityDataProvider', [***REMOVED***);
-
-        /*
-        $moreMethodsUse = $this->getExtraSetter();
-
-        if (count($moreMethodsUse)>0) {
-            foreach ($moreMethodsUse as $newMock) {
-                $classId    = str_replace('add', '', $newMock);
-                $dataProvider[***REMOVED*** = sprintf('                $%s', $this->createVar($classId));
-            }
-        }
-
-
-
-        return $dataProvider;
-        */
-    }
-
-    public function getMocks()
-    {
-        return $this->columnManager->extractCode('getEntityMock', [***REMOVED***);
-
-        /*
-        $moreMethodsUse = $this->getExtraSetter();
-
-        if (count($moreMethodsUse)>0) {
-            foreach ($moreMethodsUse as $newMock) {
-                $clearClass = str_replace('add', '', $newMock);
-                $classId    = str_replace('addId', '', $newMock);
-
-                $mock = '        ';
-                $mock .= sprintf('$%s = ', $this->createVar($clearClass));
-                $mock .= sprintf(
-                    '$this->prophesize(\'%s\\Entity\\%s\')->reveal();',
-                    $this->getModule()->getModuleName(),
-                    $this->str('class', $classId)
-                ).PHP_EOL;
-                $mocks[***REMOVED*** = $mock;
-            }
-        }
-
-        return $mocks;
-        */
-    }
-
-
-    public function getParams()
-    {
-        return $this->columnManager->extractCode('getEntityParam', [***REMOVED***, [
-            \Gear\Column\Integer\PrimaryKey::class
-        ***REMOVED***);
-
-        /*
-        $moreMethodsUse = $this->getExtraSetter();
-
-        if (count($moreMethodsUse)>0) {
-            foreach ($moreMethodsUse as $newMock) {
-                $classId    = str_replace('add', '', $newMock);
-                $params[***REMOVED*** = sprintf('        $%s', $this->str('var-length', $classId));
-            }
-        }
-        */
-
-        //return $params;
-    }
-
-    /*
-
-    public function getClassMethods()
-    {
-        $methods = get_class_methods(
-            sprintf(
-                '\%s\\Entity\\%s',
-                $this->getModule()->getModuleName(),
-                $this->str('class', $this->tableName)
-                )
-            );
-
-        if (!empty($methods)) {
-            return $methods;
-        }
-        return [***REMOVED***;
-    }
-
-    public function getExtraGetter($useMethods)
-    {
-
-        $classMethods = $this->getClassMethods();
-
-        $moreMethods = array_diff($classMethods, $useMethods);
-
-
-        $filter = function ($value) {
-            if (substr($value, 0, 3) === 'get') {
-                return true;
-            } else {
-                return false;
-            }
-        };
-
-        return array_filter($moreMethods, $filter);
-    }
-*/
 }
