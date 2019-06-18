4837d04da6dbcdb2d4b43bbc206604c05d419e81
Mauricio Piber <mauriciopiber@gmail.com>
2019-06-18 12:08:56 +0000

    constructor src service

--- src/Constructor/Src/SrcConstructorFactory.php
+++ src/Constructor/Src/SrcConstructorFactory.php
@@ -2,6 +2,24 @@
 namespace Gear\Constructor\Src;
 
 use Zend\ServiceManager\Factory\FactoryInterface;
+use Gear\Table\TableService;
+use Gear\Mvc\ViewHelper\ViewHelperService;
+use Gear\Mvc\ValueObject\ValueObjectService;
+use Gear\Mvc\TraitTestService;
+use Gear\Mvc\TraitService;
+use Gear\Mvc\Service\ServiceService;
+use Gear\Mvc\Search\SearchService;
+use Gear\Mvc\Repository\RepositoryService;
+use Gear\Mvc\InterfaceService;
+use Gear\Mvc\Form\FormService;
+use Gear\Mvc\Fixture\FixtureService;
+use Gear\Mvc\Filter\FilterService;
+use Gear\Mvc\Factory\FactoryTestService;
+use Gear\Mvc\Factory\FactoryService;
+use Gear\Mvc\Entity\EntityService;
+use Gear\Mvc\ControllerPlugin\ControllerPluginService;
+use Gear\Mvc\Config\ServiceManager;
+use Gear\Column\ColumnService;
 use Interop\Container\ContainerInterface;
 #use Gear\Module\ConstructorServiceFactory;
 use Gear\Constructor\Src;
@@ -13,26 +31,26 @@ class SrcConstructorFactory implements FactoryInterface
     public function __invoke(ContainerInterface $container, $requestedName = null, $options = [***REMOVED***)
     {
         return new SrcConstructor(
-            $container->get('Gear\Table\TableService'),
-            $container->get('Gear\Column\ColumnService'),
+            $container->get(TableService::class),
+            $container->get(ColumnService::class),
             $container->get(ModuleStructure::class),
             $container->get(SrcSchema::class),
-            $container->get('Gear\Mvc\Config\ServiceManager'),
-            $container->get('Gear\Mvc\TraitService'),
-            $container->get('Gear\Mvc\TraitTestService'),
-            $container->get('Gear\Mvc\Factory\FactoryService'),
-            $container->get('Gear\Mvc\Factory\FactoryTestService'),
-            $container->get('Gear\Mvc\Form\FormService'),
-            $container->get('Gear\Mvc\Filter\FilterService'),
-            $container->get('Gear\Mvc\Entity\EntityService'),
-            //$container->get('Gear\Mvc\Search\SearchService'),
-            $container->get('Gear\Mvc\ValueObject\ValueObjectService'),
-            $container->get('Gear\Mvc\ViewHelper\ViewHelperService'),
-            $container->get('Gear\Mvc\ControllerPlugin\ControllerPluginService'),
-            $container->get('Gear\Mvc\Repository\RepositoryService'),
-            $container->get('Gear\Mvc\Service\ServiceService'),
-            $container->get('Gear\Mvc\Fixture\FixtureService'),
-            $container->get('Gear\Mvc\InterfaceService')
+            $container->get(ServiceManager::class),
+            $container->get(TraitService::class),
+            $container->get(TraitTestService::class),
+            $container->get(FactoryService::class),
+            $container->get(FactoryTestService::class),
+            $container->get(FormService::class),
+            $container->get(FilterService::class),
+            $container->get(EntityService::class),
+            //$container->get(SearchService::class),
+            $container->get(ValueObjectService::class),
+            $container->get(ViewHelperService::class),
+            $container->get(ControllerPluginService::class),
+            $container->get(RepositoryService::class),
+            $container->get(ServiceService::class),
+            $container->get(FixtureService::class),
+            $container->get(InterfaceService::class)
         );
     }
 }
