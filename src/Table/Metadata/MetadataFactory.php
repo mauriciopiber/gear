1e5e762c160b47e25025e6cf273fb000a1a60ef3
Mauricio Piber <mauriciopiber@gmail.com>
2019-09-26 01:04:26 -0300

    working api with gear script gc

--- src/Table/Metadata/MetadataFactory.php
+++ src/Table/Metadata/MetadataFactory.php
@@ -32,19 +32,11 @@ class MetadataFactory implements FactoryInterface
         }
 
 
-        if (is_file($module->getMainFolder().'/config/autoload/global.php')
-            && $module->getMainFolder().'/config/autoload/local.php'
-        ) {
+        if (is_file($module->getMainFolder().'/config/autoload/global.php')) {
+
             $global = require $module->getMainFolder().'/config/autoload/global.php';
-            $local =  require $module->getMainFolder().'/config/autoload/local.php';
 
-            $config = array_merge_recursive($global, $local);
-
-            $params = $config['doctrine'***REMOVED***['connection'***REMOVED***['orm_default'***REMOVED***['params'***REMOVED***;
-
-            $params['driver'***REMOVED*** = 'pdo_mysql';
-
-            $params['username'***REMOVED*** = $params['user'***REMOVED***;
+            $params = $global['db'***REMOVED***;
 
             $adapter = new \Zend\Db\Adapter\Adapter($params);
         } else {
